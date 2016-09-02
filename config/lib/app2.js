83c2b6205bd20908229ed035ed4f0123224a6957
com2best <rltkd6tl!>
2016-09-02 17:13:22 +0900

    nh 대비 수정

--- config/lib/app2.js
+++ config/lib/app2.js
@@ -8,6 +8,7 @@ module.exports.start = function() {
   var bodyParser = require('body-parser');
   var kakao = require('../../modules/bot/server/controllers/kakao.server.controller');
   var action = require('../../modules/bot/server/controllers/_action.server.controller');
+  var consolidate = require('consolidate'),
 
   app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
   app.use(bodyParser.json()); // for parsing application/jsonå
