6232c3e0cbf0cd363ba6cbf375bb29c9f8237ba7
com2best <rltkd6tl!>
2016-11-04 11:57:45 +0900

    배달봇 수정

--- custom_modules/sample/demo.js
+++ custom_modules/sample/demo.js
@@ -33,44 +33,52 @@ var options = {
 };
 
 function naverTest(task, context, callback) {
-    var client = webdriverio
-        .remote(options)
-        .init()
-        .then(function() {
-            // client.end();
-            client.url('http://www.naver.com')
-                .pause(500)
-                .setValue('#query', '강남 스테이크 맛집')
-                .click('#search_btn')
-                .pause(1000)
-                .scroll('#lcs_greenmap')
-                .getText('#sp_local_2 > dl > dd:nth-child(3) > span.tell')
-                .then(function(text) {
-                    context.dialog.mobile = text;
-                    console.log(text);
-                })
-                .getText('#sp_local_2 > dl > dd:nth-child(3) > span:nth-child(2)')
-                .catch(function(err) {
-                    console.log(err);
-                })
-                .then(function(text) {
-                    context.dialog.address = text;
-                    console.log(text);
-                })
-                .then(function(){
-                    // var messages = require(path.resolve('modules/messages/server/controllers/messages.server.controller'));
-                    // var vmsMessage = "안녕하세요 카카오톡 예약봇 양얌입니다. " +
-                    //     _menuStr + ' 배달해 주세요.' +
-                    //     '주소는 ' + context.dialog.address + ' 입니다.' +
-                    //     '전화번호는 ' + context.dialog.mobile + ' 입니다.' +
-                    //     '이 전화은 인공지능 배달봇 얌얌의 카카오톡에서 예약 대행 서비스입니다.';
-                    //     messages.sendVMS({callbackPhone: '028585683', phone: '01066624995', message: vmsMessage}, context,
-                    //         function(_task, _context) {
-                                callback(task, context);
-                            // });
-                })
+  var client = webdriverio
+    .remote(options)
+    .init()
+    .then(function () {
+      // client.end();
+      client.url('http://www.naver.com')
+        .pause(500)
+        .setValue('#query', '강남 스테이크 맛집')
+        .click('#search_btn')
+        .pause(1000)
+        .scroll('#lcs_greenmap')
+        .getText('#sp_local_2 > dl > dd:nth-child(3) > span.tell')
+        .then(function (text) {
+          context.dialog.mobile = text;
+          console.log(text);
         })
-};
+        .getText('#sp_local_2 > dl > dd:nth-child(3) > span:nth-child(2)')
+        .catch(function (err) {
+          console.log(err);
+        })
+        .then(function (text) {
+          context.dialog.address = text;
+          console.log(text);
+        })
+        .then(function () {
+          var request = require('request');
+
+          var vmsMessage = "이 전화는 인공지능 예약 데모 입니다. " +
+            '아래와 같은 곳에 예약 부탁합니다.' +
+            '주소는 ' + context.dialog.address + ' 입니다.' +
+            '전화번호는 ' + context.dialog.mobile + ' 입니다.' +
+            '이 전화는 인공지능 예약 데모 입니다.';
+
+          request.post(
+            'https://bot.moneybrain.ai/api/messages/vms/send',
+            {callbackPhone: '028585683', phone: '01063165683', message: vmsMessage},
+            function (error, response, body) {
+              if (!error && response.statusCode == 200) {
+                // callback(task, context);
+              }
+              callback(task, context);
+            }
+          );
+        })
+    })
+}
 
 function samsung(task, context, callback) {
     var client = webdriverio
