0ba9a812e1e39ee350e32e5e8778fda88eb23555
com2best <rltkd6tl!>
2016-11-16 15:46:17 +0900

    배달봇 수정

--- custom_modules/order/order.bot.js
+++ custom_modules/order/order.bot.js
@@ -20,14 +20,14 @@ var orderBot = {
     CHANNEL_ACCESS_TOKEN: 'qEZFTLvs+VZc261MqfjfTWAdNPqqJA7Xwvrnbfmc8NWNJWXqoGCozNa174yDjm7gJM7/WNjcVIR2sqxFYF4LJNIDe1tdLe7ru04K53JzJYQopy3rjq872G2WLrcN/tI3CBaER6OotAv401QQnI57qgdB04t89/1O/w1cDnyilFU='
   },
   managers: [
-    // {platform: 'facebook', userId: '1240743695984292', name: '전용원'},
-    // {platform: 'facebook', userId: '1033474390107986', name: '박유진'},
+    {platform: 'facebook', userId: '1240743695984292', name: '전용원'},
+    {platform: 'facebook', userId: '1033474390107986', name: '박유진'},
     {platform: 'facebook', userId: '1314256105273043', name: '장세영'}
   ],
 
   messages: {
-    manager: false,
-    sms: false,
+    manager: true,
+    sms: true,
     call: false
   },
   // call: false,      // 배달 시 주문 전화 여부
