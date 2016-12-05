5f3af30aebdb3abc67ded9e83feb8fd0d2fe3a29
com2best <root@ip-172-31-15-9.ap-northeast-2.compute.internal>
2016-12-05 21:57:55 +0900

    merge

--- custom_modules/order/order.dialog.js
+++ custom_modules/order/order.dialog.js
@@ -158,7 +158,7 @@ var dialogs = [
         { input: {types: [{type : type.mobileType, context: false}]},
           task: {action: messages.sendSMSAuth},
           output: [
-            {if: false /*'dialog.task.result != "SUCCESS"'*/, output: {repeat: 1, options: {output: '문자 발송이 안되는데, 휴대폰 번호를 다시 말씀해주세요.'}}},
+//            {if: false /*'dialog.task.result != "SUCCESS"'*/, output: {repeat: 1, options: {output: '문자 발송이 안되는데, 휴대폰 번호를 다시 말씀해주세요.'}}},
             {output: {call: 'SMS인증'}}   // TODO output에 1개 목록 있을 때 task 두번 호출되는 현상
           ]
         },
