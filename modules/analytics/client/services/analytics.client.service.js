c3c8db9bc95b0353ed8e51a7ee078ad771adf583
spikewj <spikewj@gmail.com>
2017-03-12 04:12:07 +0900

    added filter

--- modules/analytics/client/services/analytics.client.service.js
+++ modules/analytics/client/services/analytics.client.service.js
@@ -2,60 +2,45 @@
 
 angular.module('analytics').factory('AnalyticsService', ['$resource',
   function ($resource) {
-    return $resource('api/user-count/:botUserId', {
-      botUserId: '@_id'
-    }, {
-      update: {
-        method: 'PUT'
-      }
-    });
+    return $resource('api/user-count/:kind/:arg', {
+      kind: 'kind',
+      arg: 'arg'
+    }, {});
   }
 ]);
 
 angular.module('analytics').factory('DialogUsageService', ['$resource',
   function ($resource) {
-    return $resource('api/dialog-usage/:botUserId', {
-      botUserId: '@_id'
-    }, {
-      update: {
-        method: 'PUT'
-      }
-    });
+    return $resource('api/dialog-usage/:kind/:arg', {
+      kind: 'kind',
+      arg: 'arg'
+    }, {});
   }
 ]);
 
 angular.module('analytics').factory('SessionSuccessService', ['$resource',
   function ($resource) {
-    return $resource('api/session-success/:botUserId', {
-      botUserId: '@_id'
-    }, {
-      update: {
-        method: 'PUT'
-      }
-    });
+    return $resource('api/session-success/:kind/:arg', {
+      kind: 'kind',
+      arg: 'arg'
+    }, {});
   }
 ]);
 
 angular.module('analytics').factory('DialogSuccessService', ['$resource',
   function ($resource) {
-    return $resource('api/dialog-success/:botUserId', {
-      botUserId: '@_id'
-    }, {
-      update: {
-        method: 'PUT'
-      }
-    });
+    return $resource('api/dialog-success/:kind/:arg', {
+      kind: 'kind',
+      arg: 'arg'
+    }, {});
   }
 ]);
 
 angular.module('analytics').factory('DialogFailureService', ['$resource',
   function ($resource) {
-    return $resource('api/dialog-failure/:botUserId', {
-      botUserId: '@_id'
-    }, {
-      update: {
-        method: 'PUT'
-      }
-    });
+    return $resource('api/dialog-failure/:kind/:arg', {
+      kind: 'kind',
+      arg: 'arg'
+    }, {});
   }
 ]);
--- modules/analytics/server/routes/analytics.server.routes.js
+++ modules/analytics/server/routes/analytics.server.routes.js
@@ -8,19 +8,19 @@ var analyticsPolicy = require('../policies/analytics.server.policy'),
 
 module.exports = function(app) {
   // Bot users Routes
-  app.route('/api/user-count').all(analyticsPolicy.isAllowed)
+  app.route('/api/user-count/:kind/:arg').all(analyticsPolicy.isAllowed)
     .get(analytics.list);
 
-  app.route('/api/dialog-usage').all(analyticsPolicy.isAllowed)
+  app.route('/api/dialog-usage/:kind/:arg').all(analyticsPolicy.isAllowed)
     .get(analytics.dialogList);
 
-  app.route('/api/dialog-success').all(analyticsPolicy.isAllowed)
+  app.route('/api/dialog-success/:kind/:arg').all(analyticsPolicy.isAllowed)
     .get(analytics.dialogSuccessList);
 
-  app.route('/api/session-success').all(analyticsPolicy.isAllowed)
+  app.route('/api/session-success/:kind/:arg').all(analyticsPolicy.isAllowed)
     .get(analytics.sessionSuccessList);
 
-  app.route('/api/dialog-failure').all(analyticsPolicy.isAllowed)
+  app.route('/api/dialog-failure/:kind/:arg').all(analyticsPolicy.isAllowed)
     .get(analytics.dialogFailureList);
 
   // Finish by binding the Bot user middleware
