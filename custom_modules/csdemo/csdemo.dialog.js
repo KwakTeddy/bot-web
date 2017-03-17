e34a78973f63ec91cd24b44fd696fea2cf40b482
com2best <rltkd6tl!>
2017-03-17 22:47:48 +0900

    플레이챗 경로 /, 관리자 경로 /developer, 로그인 창 들어갈때 사라짐 수정

--- custom_modules/csdemo/csdemo.dialog.js
+++ custom_modules/csdemo/csdemo.dialog.js
@@ -1425,9 +1425,9 @@ _bot.setDialogs(dialogs);
 _bot.setCommonDialogs(commonDialogs);
 
 // TEST
-var json = JSON.stringify(dialogs);
-console.log(json);
-var fs = require('fs');
-fs.writeFile(require('path').resolve("public/js") + "/dialog.json", json, function(err) {
-if(err) { return console.log(err); }
-console.log("dialog.json was saved!"); });
+// var json = JSON.stringify(dialogs);
+// console.log(json);
+// var fs = require('fs');
+// fs.writeFile(require('path').resolve("public/js") + "/dialog.json", json, function(err) {
+// if(err) { return console.log(err); }
+// console.log("dialog.json was saved!"); });
