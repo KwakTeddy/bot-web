9ddb15ef37e42b427b55af0a831f943bf92fc44c
junhapark <5709psy@gmail.com>
2017-03-21 00:39:12 +0900

    오류 확인

--- modules/bots/client/controllers/user-bot.client.controller.js
+++ modules/bots/client/controllers/user-bot.client.controller.js
@@ -201,7 +201,7 @@ angular.module('user-bots').controller('UserBotController', ['$scope', '$rootSco
         FB.api('me/subscribed_apps?access_token='+ page.access_token, 'post', function (response) {
             console.log(response)
         });
-        FB.api('240853479709635/subscriptions', 'post', {object: page, callback_url: '/api/facebook/'+ '123' +'/webhook'}, function (response) {
+        FB.api('240853479709635/subscriptions', 'post', {object: 'page', callback_url: '/api/facebook/'+ '123' +'/webhook'}, function (response) {
             console.log(response)
         })
       };
