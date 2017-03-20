7b50d4b17592ab050b5b7e342a2d6f705bedc0c8
spikewj <spikewj@gmail.com>
2017-03-20 19:05:29 +0900

    revised production config

--- config/assets/production.js
+++ config/assets/production.js
@@ -1,25 +1,69 @@
 'use strict';
 
 module.exports = {
-  //client: {
-  //  lib: {
-  //    css: [
-  //      'public/lib/bootstrap/dist/css/bootstrap.min.css',
-  //      'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
-  //    ],
-  //    js: [
-  //      'public/lib/angular/angular.min.js',
-  //      'public/lib/angular-resource/angular-resource.min.js',
-  //      'public/lib/angular-animate/angular-animate.min.js',
-  //      'public/lib/angular-messages/angular-messages.min.js',
-  //      'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-  //      'public/lib/angular-ui-utils/ui-utils.min.js',
-  //      'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-  //      'public/lib/angular-file-upload/angular-file-upload.min.js',
-  //      'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
-  //    ]
-  //  },
-  //  css: 'public/dist/application.min.css',
-  //  js: 'public/dist/application.min.js'
-  //}
+  client: {
+    lib: {
+      css: [
+        'public/lib/bootstrap/dist/css/bootstrap.css',
+        'public/lib/bootstrap/dist/css/bootstrap-theme.css'
+      ],
+      js: [
+        'public/lib/angular/angular.js',
+        'public/lib/angular-datatables/dist/angular-datatables.js',
+        'public/lib/angular-resource/angular-resource.js',
+        'public/lib/angular-animate/angular-animate.js',
+        'public/lib/angular-cookies/angular-cookies.js',
+        'public/lib/angular-messages/angular-messages.js',
+        'public/lib/angular-ui-router/release/angular-ui-router.js',
+        'public/lib/angular-ui-utils/ui-utils.js',
+        'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+        'public/lib/angular-file-upload/dist/angular-file-upload.js',
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/codemirror/lib/codemirror.js',
+        'public/lib/codemirror/mode/javascript/javascript.js',
+        'public/lib/angular-ui-codemirror/ui-codemirror.js',
+        'public/lib/angular-ui-select/dist/select.js',
+        'public/lib/angular-sanitize/angular-sanitize.js',
+        'public/lib/dropzone/downloads/dropzone.js',
+        'public/lib/angular-dropzone/lib/angular-dropzone.js',
+        'public/lib/ngInfiniteScroll/build/ng-infinite-scroll.js',
+        'public/lib/angular-dropzone/lib/angular-dropzone.js',
+        'public/lib/d3/d3.min.js',
+        'public/lib/d3-tip/index.js',
+        'public/lib/ionic/js/ionic.bundle.js'
+      ],
+      tests: ['public/lib/angular-mocks/angular-mocks.js']
+    },
+    css: 'public/dist/application.min.css',
+    js: 'public/dist/application.min.js'
+  },
+  mobile: {
+    lib: {
+      css: [
+        'public/lib/ionic/css/ionic.app.css'
+      ],
+      js: [
+        'public/lib/ionic/js/ionic.bundle.js',
+        'public/lib/ngCordova/dist/ng-cordova.min.js',
+        'public/lib/angular-resource/angular-resource.js',
+        //'public/lib/angular-animate/angular-animate.js',
+        'public/lib/angular-messages/angular-messages.js',
+        'public/lib/angular-ui-router/release/angular-ui-router.js',
+        'public/lib/angular-ui-utils/ui-utils.js',
+        'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+        'public/lib/angular-cookies/angular-cookies.min.js',
+        'public/lib/ngInfiniteScroll/build/ng-infinite-scroll.js',
+        // 'public/lib/angular-file-upload/angular-file-upload.js',
+        // 'public/lib/ng-file-upload/ng-file-upload-shim.js',
+        // 'public/lib/ng-file-upload/ng-file-upload.js',
+
+        // 'http://dmaps.daum.net/map_js_init/postcode.v2.js',
+        // 'https://www.youtube.com/iframe_api',
+        // 'https://developers.kakao.com/sdk/js/kakao.min.js',
+        // 'public/lib/angular-update-meta/dist/update-meta.min.js'
+      ]
+    },
+    css: 'public/dist/application_mobile.min.css',
+    js: 'public/dist/application_mobile.min.js'
+  }
 };
