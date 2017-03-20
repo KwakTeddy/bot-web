501ce60600280623e7f01d7ad976a6219e3f5469
junhapark <5709psy@gmail.com>
2017-03-21 01:24:07 +0900

    오류 확인

--- modules/bots/client/controllers/user-bot.client.controller.js
+++ modules/bots/client/controllers/user-bot.client.controller.js
@@ -177,20 +177,20 @@ angular.module('user-bots').controller('UserBotController', ['$scope', '$rootSco
       $scope.userBotId = vm.userBot.id;
 
       if ((channel == 'facebook') && (method !== 'easy')){
-        FB.api('/me/accounts?fields=picture,name,link,access_token', function(response) {
-          console.log(response);
-          if (response.error && (response.error.code == 2500)){
-            var url = '/api/auth/facebook/page';
-            if ($state.previous && $state.previous.href) {
-                url += '?redirect_to=' + encodeURIComponent($state.previous.href);
-            }
-            // Effectively call OAuth authentication route:
-              console.log(url);
-            $window.location.href = url;
-          }
-          $scope.pageList = [];
-          $scope.pageList = response.data;
-        });
+        // FB.api('/me/accounts?fields=picture,name,link,access_token', function(response) {
+        //   console.log(response);
+        //   if (response.error && (response.error.code == 2500)){
+        //     var url = '/api/auth/facebook/page';
+        //     if ($state.previous && $state.previous.href) {
+        //         url += '?redirect_to=' + encodeURIComponent($state.previous.href);
+        //     }
+        //     // Effectively call OAuth authentication route:
+        //       console.log(url);
+        //     $window.location.href = url;
+        //   }
+        //   $scope.pageList = [];
+        //   $scope.pageList = response.data;
+        // });
       }
       $scope.close = function () {
           modalInstance.dismiss();
