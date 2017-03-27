a7214cc329a9df48bda9fdcb8ba00d4976d02768
spikewj <spikewj@gmail.com>
2017-03-27 17:16:20 +0900

    fixed

--- config/lib/bot.js
+++ config/lib/bot.js
@@ -92,11 +92,12 @@ function loadBot(botName) {
 
   bot.setDialogs(globalDialogs.globalStartDialogs);
 
+  var filePaths = [];
   if(bot && bot.dialogFiles) {
     for (var i = 0; i < bot.dialogFiles.length; i++) {
       var file = bot.dialogFiles[i];
       var filePath = path.join(botDir, file);
-      //bot.dialogFiles[i] = filePath;
+      filePaths.push(filePath);
 
       try {
         console.log('\tloading file: ' + file);
@@ -110,7 +111,7 @@ function loadBot(botName) {
   }
 
   fileFilter = function(file) {
-    if(bot && bot.dialogFiles && _.includes(path.join(botDir, bot.dialogFiles), file)) return false;
+    if(bot && bot.dialogFiles && _.includes(filePaths, file)) return false;
     else return file.endsWith('.dialog.js');
   };
 
@@ -144,7 +145,7 @@ function loadBot(botName) {
   // bot.setDialogs([dialogsetModule.faqDialog]);
 
   fileFilter = function(file) {
-    if(bot && bot.dialogFiles && _.includes(path.join(botDir, bot.dialogFiles), file)) return false;
+    if(bot && bot.dialogFiles && _.includes(filePaths, file)) return false;
 
     else if(file.endsWith('.js') && !file.endsWith('.dialog.js') && !file.endsWith('.bot.js')) {
       // var jsPath = path.resolve('custom_modules/' + botName + '/' + file);
