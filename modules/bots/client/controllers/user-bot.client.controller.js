67a4e2175949f8d70e40410722831fb300afc160
spikewj <spikewj@gmail.com>
2017-04-06 14:52:47 +0900

    added special handling for address

--- modules/bots/client/controllers/user-bot.client.controller.js
+++ modules/bots/client/controllers/user-bot.client.controller.js
@@ -8,16 +8,6 @@ var setInput = function(cur) {
   currentNode = cur.replace(/_/g,'.');
 };
 
-var findAddress = function() {
-  new daum.Postcode({
-    oncomplete: function(data) {
-      // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분입니다.
-      // 예제를 참고하여 다양한 활용법을 확인해 보세요.
-      document.getElementsByName("root[address]")[0].value = data.address;
-    }
-  }).open();
-};
-
 if (_platform !== 'mobile'){
   $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
@@ -42,6 +32,19 @@ if (_platform !== 'mobile'){
       } else {
         vm.userBot.public = true;
       }
+
+      vm.findAddress = function() {
+        new daum.Postcode({
+          oncomplete: function(data) {
+            // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분입니다.
+            // 예제를 참고하여 다양한 활용법을 확인해 보세요.
+            //document.getElementsByName("address1")[0].value = data.address;
+            $scope.$apply(function () {
+              vm.address1 = data.address;
+            });
+          }
+        }).open();
+      };
 
       vm.userId = $rootScope.userId;
       vm.isMine = (vm.userBot.user != null && (vm.user.username === vm.userBot.user.username));
@@ -456,6 +459,7 @@ if (_platform !== 'mobile'){
             //TODO: move to template definition
             if (key === 'address') {
               schema[key]["options"] = {grid_columns: 12};
+              schema[key]["readonly"] = true;
             }
             if (key === 'startTime')
               schema[key]["default"] = '09:00';
@@ -511,6 +515,17 @@ if (_platform !== 'mobile'){
             return errors;
           }
 
+          if (path === "root.address") {
+            if (value === "") {
+              errors.push({
+                path: path,
+                property: 'format',
+                message: schema.title + "를 입력해주세요"
+              });
+              return errors;
+            }
+          }
+
           if (schema.format !== "image" && value === "") {
             // Errors must be an object with `path`, `property`, and `message`
             var msg;
@@ -559,14 +574,37 @@ if (_platform !== 'mobile'){
           grid_columns: 3,
         });
 
+        // special handling for address
         if (document.getElementsByName("root[address]").length == 1) {
-          var innerHTML = '&nbsp;&nbsp;&nbsp;<button class="btn btn-default" onClick="javascript:findAddress()" >주소검색</button>';
+          var innerHTML = '&nbsp;&nbsp;&nbsp;<button class="btn btn-default" ng-click="vm.findAddress()" >주소검색</button>';
           document.getElementsByName("root[address]")[0].insertAdjacentHTML("beforebegin", innerHTML);
+          innerHTML = '<div name="mine" class="row" style="padding-top:10px">';
+          innerHTML += '<div class="col-md-7"><input ng-model="vm.address1" type="text" class="form-control" name="address1" disabled></div>';
+          innerHTML += '<div class="col-md-5"><input ng-model="vm.address2" ng-disabled="!vm.address1" type="text" class="form-control" name="address2"></div>';
+          innerHTML += "</div>";
+          document.getElementsByName("root[address]")[0].insertAdjacentHTML("beforebegin", innerHTML);
+          document.getElementsByName("root[address]")[0].style.display = "none";
         }
         $compile(document.getElementById('editor_holder'))($scope);
         if (vm.userBot.templateId === template._id) {
           console.log("given input=" + JSON.stringify(vm.userBot.templateData));
           editor.setValue(vm.userBot.templateData);
+        }
+
+        vm.address1 = "";
+        vm.address2 = "";
+
+        $scope.$watch('vm.address2', function () {
+          if (vm.address2 && vm.address2 != "")
+            editor.getEditor("root.address").setValue(vm.address1 + ", " + vm.address2);
+          else
+            editor.getEditor("root.address").setValue("");
+        });
+
+        if (vm.userBot.templateData && vm.userBot.templateData.address) {
+          var addrs = vm.userBot.templateData.address.split(',');
+          vm.address1 = addrs[0];
+          vm.address2 = addrs[1];
         }
 
         editor.on('change', function() {
