aa673fe2fed1e7bf7692ece443e2ae6084029d5c
junhapark <5709psy@gmail.com>
2017-04-11 18:37:49 +0900

    네이버톡

--- modules/bot/server/controllers/navertalk.server.controller.js
+++ modules/bot/server/controllers/navertalk.server.controller.js
@@ -10,14 +10,50 @@ exports.message =  function(req, res) {
   console.log("navertalk message");
   console.log(JSON.stringify(req.body));
 
-  // if(req.body && req.body.user_key && req.body.content) {
-  //   var from = req.body.user_key;
-  //   var type = req.body.type;
-  //   var text = req.body.content;
-  //
-  //   console.log(JSON.stringify(req.params));
-  //   chat.write('kakao', from, req.params.bot, text, function (serverText, json) {
-  //     respondMessage(res, serverText, json)
-  //   });
-  // }
+  if(req.body && req.body.username && req.body.text) {
+    var from = req.body.username;
+    var text = req.body.text;
+
+    console.log(JSON.stringify(req.params));
+    chat.write('navertalk', from, req.params.bot, text, function (serverText, json) {
+      respondMessage(res, serverText, json)
+    });
+  }
 };
+
+function respondMessage(res, text, json) {
+  var sendMsg =
+    {
+      "message": {
+        "text": text
+      }
+    };
+
+  if(json && json.photoUrl) {
+    sendMsg.message.photo = {
+      "url": json.photoUrl,
+      "width": json.photoWidth,
+      "height":json.photoHeight
+    }
+  }
+
+  if(json && json.url) {
+    sendMsg.message.message_button =
+      {
+        "label": (json.urlMessage ? json.urlMessage : "상세정보보기"),
+        "url": json.url
+      };
+  }
+
+  if(json && json.buttons) {
+    sendMsg.keyboard =
+      {
+        "type": "buttons",
+        "buttons": json.buttons
+      };
+  }
+
+  console.log(JSON.stringify(sendMsg));
+  res.write(JSON.stringify(sendMsg));
+  res.end();
+}
