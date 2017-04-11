4cfadaea054b8451359c5b4dbb7cbe4f6c2488b2
junhapark <5709psy@gmail.com>
2017-04-11 18:29:02 +0900

    네이버톡

--- modules/bot/server/controllers/navertalk.server.controller.js
+++ modules/bot/server/controllers/navertalk.server.controller.js
@@ -7,7 +7,17 @@ var contextModule = require(path.resolve('modules/bot/engine/common/context'));
 var util =require('util'); //temporary
 
 exports.message =  function(req, res) {
-  console.log(req.body);
-  console.log('lol');
-  res.status(200).end();
+  console.log("navertalk message");
+  console.log(JSON.stringify(req.body));
+
+  // if(req.body && req.body.user_key && req.body.content) {
+  //   var from = req.body.user_key;
+  //   var type = req.body.type;
+  //   var text = req.body.content;
+  //
+  //   console.log(JSON.stringify(req.params));
+  //   chat.write('kakao', from, req.params.bot, text, function (serverText, json) {
+  //     respondMessage(res, serverText, json)
+  //   });
+  // }
 };
