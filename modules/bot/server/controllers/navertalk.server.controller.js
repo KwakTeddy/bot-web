10252244d6d3fb8898defec6f3f1915383459faa
junhapark <5709psy@gmail.com>
2017-04-11 18:23:30 +0900

    인기봇 최신봇 친구봇 선택 탭 알기

--- modules/bot/server/controllers/navertalk.server.controller.js
+++ modules/bot/server/controllers/navertalk.server.controller.js
@@ -7,6 +7,7 @@ var contextModule = require(path.resolve('modules/bot/engine/common/context'));
 var util =require('util'); //temporary
 
 exports.message =  function(req, res) {
+  console.log(req.body);
   console.log('lol');
+  res.status(200).end();
 };
-
