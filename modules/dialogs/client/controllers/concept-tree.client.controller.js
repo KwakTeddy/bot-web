8af0f8801af421cd8dfacaffbf29ad7d572fdecd
spikewj <spikewj@gmail.com>
2017-04-13 17:13:52 +0900

    cleaned up concept graph

--- modules/dialogs/client/controllers/concept-tree.client.controller.js
+++ modules/dialogs/client/controllers/concept-tree.client.controller.js
@@ -14,7 +14,7 @@ angular.module('dialogsets').controller('ConceptTreeController', ['$scope', '$ro
     treeData = {name: 'head', children: []};
     $resource('/api/conceptlist', {}).query({}, function(res) {
       for(var i = 0; i < res.length; i++) {
-        nodes[res[i]._id] = {name: res[i].name, children:[], _children:null};
+        nodes[res[i]._id] = {name: res[i].name, children:[]};
         if (res[i].parent == null) {
           if (!nodes[res[i].name]) {
             nodes[res[i].name] = nodes[res[i]._id];
@@ -23,42 +23,30 @@ angular.module('dialogsets').controller('ConceptTreeController', ['$scope', '$ro
             nodes[res[i]._id] = nodes[res[i].name];
           }
         } else {
-          // if (nodes[nodes[res[i].parent].name]) {
-          //   nodes[res[i].parent].children.push(nodes[res[i]._id]);
-          // }
-          // else
-          {
-            if (nodes[res[i].parent]._children == null) {
-              nodes[res[i].parent].children = null;
-              nodes[res[i].parent]._children = [];
-            }
-            nodes[res[i].parent]._children.push(nodes[res[i]._id]);
-          }
+          nodes[res[i].parent].children.push(nodes[res[i]._id]);
         }
       }
       $resource('/api/lgfaq', {}).query({}, function(res) {
         for(var i = 0; i < res.length; i++) {
-          if (nodes[res[i].conceptId]._children == null) {
-            nodes[res[i].conceptId]._children = [];
-            nodes[res[i].conceptId]._children.push({input: '', count: 0});
-          }
-          if (nodes[res[i].conceptId]._children[0].count++ < 7)
-            nodes[res[i].conceptId]._children[0].input += '\n' + res[i].title;
+          if (nodes[res[i].conceptId].children.length < 7)
+            nodes[res[i].conceptId].children.push({name: res[i].title});
         }
+        treeData.children.forEach(function (d) {
+          d.children.forEach(function(e) {
+            collapse(e);
+          });
+        });
         init();
       });
     });
 
-
-
-    var dragStarted;
+    // height for one node
+    var itemHeight = 25;
 
     // Calculate total nodes, max label length
     var totalNodes = 0;
     var maxLabelLength = 0;
-    // variables for drag/drop
-    var selectedNode = null;
-    var draggingNode = null;
+
     // panning variables
     var panSpeed = 200;
     var panBoundary = 20; // Within 20px from edges will pan when dragging.
@@ -95,99 +83,13 @@ angular.module('dialogsets').controller('ConceptTreeController', ['$scope', '$ro
       }
     }
 
-
-    // sort the tree according to the node names
-
-    function sortTree() {
-      tree.sort(function (a, b) {
-        if (b.name == undefined || a.name == undefined)
-          return true;
-        return b.name.toLowerCase() < a.name.toLowerCase() ? 1 : -1;
-      });
-    }
-
-    // Sort the tree initially incase the JSON isn't in a sorted order.
-    //sortTree();
-
-    // TODO: Pan function, can be better implemented.
-    var panTimer;
-    function pan(domNode, direction) {
-      var speed = panSpeed;
-      if (panTimer) {
-        clearTimeout(panTimer);
-        var translateCoords = d3.transform(svgGroup.attr("transform"));
-        var translateX, translateY;
-        if (direction == 'left' || direction == 'right') {
-          translateX = direction == 'left' ? translateCoords.translate[0] + speed : translateCoords.translate[0] - speed;
-          translateY = translateCoords.translate[1];
-        } else if (direction == 'up' || direction == 'down') {
-          translateX = translateCoords.translate[0];
-          translateY = direction == 'up' ? translateCoords.translate[1] + speed : translateCoords.translate[1] - speed;
-        }
-        var scaleX = translateCoords.scale[0];
-        var scaleY = translateCoords.scale[1];
-        scale = zoomListener.scale();
-        svgGroup.transition().attr("transform", "translate(" + translateX + "," + translateY + ")scale(" + scale + ")");
-        d3.select(domNode).select('g.node').attr("transform", "translate(" + translateX + "," + translateY + ")");
-        zoomListener.scale(zoomListener.scale());
-        zoomListener.translate([translateX, translateY]);
-        panTimer = setTimeout(function () {
-          pan(domNode, speed, direction);
-        }, 50);
-      }
-    }
-
     // Define the zoom function for the zoomable tree
-
     function zoom() {
       svgGroup.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
     }
 
-
     // define the zoomListener which calls the zoom function on the "zoom" event constrained within the scaleExtents
     var zoomListener = d3.behavior.zoom().scaleExtent([0.1, 3]).on("zoom", zoom);
-
-    function initiateDrag(d, domNode) {
-      draggingNode = d;
-      d3.select(domNode).select('.ghostCircle').attr('pointer-events', 'none');
-      d3.selectAll('.ghostCircle').attr('class', 'ghostCircle show');
-      d3.select(domNode).attr('class', 'node activeDrag');
-
-      svgGroup.selectAll("g.node").sort(function (a, b) { // select the parent and sort the path's
-        if (a.id != draggingNode.id) return 1; // a is not the hovered element, send "a" to the back
-        else return -1; // a is the hovered element, bring "a" to the front
-      });
-      // if nodes has children, remove the links and nodes
-      if (nodes.length > 1) {
-        // remove link paths
-        links = tree.links(nodes);
-        var nodePaths = svgGroup.selectAll("path.link")
-          .data(links, function (d) {
-            return d.target.id;
-          }).remove();
-        // remove child nodes
-        var nodesExit = svgGroup.selectAll("g.node")
-          .data(nodes, function (d) {
-            return d.id;
-          }).filter(function (d, i) {
-            if (d.id == draggingNode.id) {
-              return false;
-            }
-            return true;
-          }).remove();
-      }
-
-      // remove parent link
-      var parentLink = tree.links(tree.nodes(draggingNode.parent));
-      svgGroup.selectAll('path.link').filter(function (d, i) {
-        if (d.target.id == draggingNode.id) {
-          return true;
-        }
-        return false;
-      }).remove();
-
-      dragStarted = null;
-    }
 
     // define the baseSvg, attaching a class for styling and the zoomListener
     var baseSvg = d3.select("#tree-container").append("svg")
@@ -196,103 +98,31 @@ angular.module('dialogsets').controller('ConceptTreeController', ['$scope', '$ro
       .attr("class", "overlay")
       .call(zoomListener);
 
+    // Append a group which holds all nodes and which the zoom Listener can act upon.
+    var svgGroup = baseSvg.append("g");
 
-    // Define the drag listeners for drag/drop behaviour of nodes.
-    var domNode;
-    var dragListener = d3.behavior.drag()
-      .on("dragstart", function (d) {
-        if (d == root) {
-          return;
-        }
-        dragStarted = true;
-        var nodes = tree.nodes(d);
-        d3.event.sourceEvent.stopPropagation();
-        // it's important that we suppress the mouseover event on the node being dragged. Otherwise it will absorb the mouseover event and the underlying node will not detect it d3.select(this).attr('pointer-events', 'none');
-      })
-      .on("drag", function (d) {
-        if (d == root) {
-          return;
-        }
+    var init = function() {
+      // Define the root
+      root = treeData;
+      root.x0 = viewerHeight / 2;
+      root.y0 = 0;
 
-        if (dragStarted) {
-          domNode = this;
-          initiateDrag(d, domNode);
-        }
+      // Call visit function to establish maxLabelLength
+      visit(treeData, function (d) {
+        totalNodes++;
+        if (d.name != undefined)
+          maxLabelLength = Math.max(d.name.length, maxLabelLength);
 
-        // get coords of mouseEvent relative to svg container to allow for panning
-        var relCoords = d3.mouse($('svg').get(0));
-        if (relCoords[0] < panBoundary) {
-          panTimer = true;
-          pan(this, 'left');
-        } else if (relCoords[0] > ($('svg').width() - panBoundary)) {
-
-          panTimer = true;
-          pan(this, 'right');
-        } else if (relCoords[1] < panBoundary) {
-          panTimer = true;
-          pan(this, 'up');
-        } else if (relCoords[1] > ($('svg').height() - panBoundary)) {
-          panTimer = true;
-          pan(this, 'down');
-        } else {
-          try {
-            clearTimeout(panTimer);
-          } catch (e) {
-
-          }
-        }
-
-        d.x0 += d3.event.dy;
-        d.y0 += d3.event.dx;
-        var node = d3.select(this);
-        node.attr("transform", "translate(" + d.y0 + "," + d.x0 + ")");
-        updateTempConnector();
-      }).on("dragend", function (d) {
-        if (d == root) {
-          return;
-        }
-        domNode = this;
-        if (selectedNode) {
-          // now remove the element from the parent, and insert it into the new elements children
-          var index = draggingNode.parent.children.indexOf(draggingNode);
-          if (index > -1) {
-            draggingNode.parent.children.splice(index, 1);
-          }
-          if (typeof selectedNode.children !== 'undefined' || typeof selectedNode._children !== 'undefined') {
-            if (typeof selectedNode.children !== 'undefined') {
-              selectedNode.children.push(draggingNode);
-            } else {
-              selectedNode._children.push(draggingNode);
-            }
-          } else {
-            selectedNode.children = [];
-            selectedNode.children.push(draggingNode);
-          }
-          // Make sure that the node being added to is expanded so user can see added node is correctly moved
-          expand(selectedNode);
-          //sortTree();
-          endDrag();
-        } else {
-          endDrag();
-        }
+      }, function (d) {
+        return d.children && d.children.length > 0 ? d.children : null;
       });
 
-    function endDrag() {
-      selectedNode = null;
-      d3.selectAll('.ghostCircle').attr('class', 'ghostCircle');
-      d3.select(domNode).attr('class', 'node');
-      // now restore the mouseover event or we won't be able to drag a 2nd time
-      d3.select(domNode).select('.ghostCircle').attr('pointer-events', '');
-      updateTempConnector();
-      if (draggingNode !== null) {
-        update(root);
-        centerNode(draggingNode);
-        draggingNode = null;
-      }
-    }
+      // Layout the tree initially and center on the root node.
+      update(root);
+      centerNode(root);
+    };
 
     // Helper functions for collapsing and expanding nodes.
-
     function collapse(d) {
       if (d.children) {
         d._children = d.children;
@@ -309,60 +139,7 @@ angular.module('dialogsets').controller('ConceptTreeController', ['$scope', '$ro
       }
     }
 
-    var overCircle = function (d) {
-      selectedNode = d;
-      updateTempConnector();
-    };
-    var outCircle = function (d) {
-      selectedNode = null;
-      updateTempConnector();
-    };
-
-    // Function to update the temporary connector indicating dragging affiliation
-    var updateTempConnector = function () {
-      var data = [];
-      if (draggingNode !== null && selectedNode !== null) {
-        // have to flip the source coordinates since we did this for the existing connectors on the original tree
-        data = [{
-          source: {
-            x: selectedNode.y0,
-            y: selectedNode.x0
-          },
-          target: {
-            x: draggingNode.y0,
-            y: draggingNode.x0
-          }
-        }];
-      }
-      var link = svgGroup.selectAll(".templink").data(data);
-
-      link.enter().append("path")
-        .attr("class", "templink")
-        .attr("d", d3.svg.diagonal())
-        .attr('pointer-events', 'none');
-
-      link.attr("d", d3.svg.diagonal());
-
-      link.exit().remove();
-    };
-
-    // Function to center node when clicked/dropped so node doesn't get lost when collapsing/moving with large amount of children.
-
-    function centerNode(source) {
-      var scale = zoomListener.scale();
-      var x = -source.y0;
-      var y = -source.x0;
-      x = x * scale + viewerWidth / 2;
-      y = y * scale + viewerHeight / 2;
-      d3.select('g').transition()
-        .duration(duration)
-        .attr("transform", "translate(" + x + "," + y + ")scale(" + scale + ")");
-      zoomListener.scale(scale);
-      zoomListener.translate([x, y]);
-    }
-
     // Toggle children function
-
     function toggleChildren(d) {
       if (d.children) {
         d._children = d.children;
@@ -375,12 +152,25 @@ angular.module('dialogsets').controller('ConceptTreeController', ['$scope', '$ro
     }
 
     // Toggle children on click.
-
     function click(d) {
       if (d3.event.defaultPrevented) return; // click suppressed
       d = toggleChildren(d);
       update(d);
       centerNode(d);
+    }
+
+    // Function to center node when clicked/dropped so node doesn't get lost when collapsing/moving with large amount of children.
+    function centerNode(source) {
+      var scale = zoomListener.scale();
+      var x = -source.y0;
+      var y = -source.x0;
+      x = x * scale + viewerWidth / 2;
+      y = y * scale + viewerHeight / 2;
+      d3.select('g').transition()
+        .duration(duration)
+        .attr("transform", "translate(" + x + "," + y + ")scale(" + scale + ")");
+      zoomListener.scale(scale);
+      zoomListener.translate([x, y]);
     }
 
     function update(source) {
@@ -400,7 +190,7 @@ angular.module('dialogsets').controller('ConceptTreeController', ['$scope', '$ro
         }
       };
       childCount(0, root);
-      var newHeight = d3.max(levelWidth) * 80; // 25 pixels per line
+      var newHeight = d3.max(levelWidth) * itemHeight;
       tree = tree.size([newHeight, viewerWidth]);
 
       // Compute the new tree layout.
@@ -424,7 +214,6 @@ angular.module('dialogsets').controller('ConceptTreeController', ['$scope', '$ro
 
       // Enter any new nodes at the parent's previous position.
       var nodeEnter = node.enter().append("g")
-        //.call(dragListener)
         .attr("class", "node")
         .attr("transform", function (d) {
           return "translate(" + source.y0 + "," + source.x0 + ")";
@@ -440,16 +229,10 @@ angular.module('dialogsets').controller('ConceptTreeController', ['$scope', '$ro
 
       nodeEnter.append("text")
         .attr("x", function (d) {
-          //return d.children || d._children ? -10 : 10;
           return 10;
         })
         .attr("dy", ".35em")
         .attr('class', 'nodeText')
-        /*
-         .attr("text-anchor", function(d) {
-         return d.children || d._children ? "end" : "start";
-         })
-         */
         .text(function (d) {
           return d.name;
         })
@@ -465,44 +248,11 @@ angular.module('dialogsets').controller('ConceptTreeController', ['$scope', '$ro
         })
         .call(wrap, 100, 1);
 
-      nodeEnter.append('text')
-        .attr('x', 10)
-        .attr("dy", "2.7em")
-        .text(function (d) {
-          if (d.output != undefined && typeof d.output === 'string')
-            return "out:" + (d.output.length > 50 ? d.output.substring(0, 50) + "..." : d.output);
-        });
-
-      // phantom node to give us mouseover in a radius around it
-      nodeEnter.append("circle")
-        .attr('class', 'ghostCircle')
-        .attr("r", 30)
-        .attr("opacity", 0.2) // change this to zero to hide the target area
-        .style("fill", "red")
-        .attr('pointer-events', 'mouseover')
-        .on("mouseover", function (node) {
-          overCircle(node);
-        })
-        .on("mouseout", function (node) {
-          outCircle(node);
-        });
-
       // Update the text to reflect whether node has children or not.
       node.select('text')
-      /*
-       .attr("x", function(d) {
-       return d.children || d._children ? -10 : 10;
-       })
-       */
         .text(function (d) {
           return d.name;
-        })
-      /*
-       .attr("text-anchor", function(d) {
-       return d.children || d._children ? "end" : "start";
-       })
-       */
-      ;
+        });
 
       // Change the circle fill depending on whether it has children and is collapsed
       node.select("circle.nodeCircle")
@@ -602,30 +352,34 @@ angular.module('dialogsets').controller('ConceptTreeController', ['$scope', '$ro
         }
       });
     }
-    // Append a group which holds all nodes and which the zoom Listener can act upon.
-    var svgGroup = baseSvg.append("g");
 
-    var init = function() {
-
-      // Define the root
-      root = treeData;
-      root.x0 = viewerHeight / 2;
-      root.y0 = 0;
-
-      // Call visit function to establish maxLabelLength
-      visit(treeData, function (d) {
-        totalNodes++;
-        if (d.name != undefined)
-          maxLabelLength = Math.max(d.name.length, maxLabelLength);
-
-      }, function (d) {
-        return d.children && d.children.length > 0 ? d.children : null;
-      });
-
-      // Layout the tree initially and center on the root node.
-      update(root);
-      centerNode(root);
-    };
+    // TODO: Pan function, can be better implemented.
+    var panTimer;
+    function pan(domNode, direction) {
+      var speed = panSpeed;
+      if (panTimer) {
+        clearTimeout(panTimer);
+        var translateCoords = d3.transform(svgGroup.attr("transform"));
+        var translateX, translateY;
+        if (direction == 'left' || direction == 'right') {
+          translateX = direction == 'left' ? translateCoords.translate[0] + speed : translateCoords.translate[0] - speed;
+          translateY = translateCoords.translate[1];
+        } else if (direction == 'up' || direction == 'down') {
+          translateX = translateCoords.translate[0];
+          translateY = direction == 'up' ? translateCoords.translate[1] + speed : translateCoords.translate[1] - speed;
+        }
+        var scaleX = translateCoords.scale[0];
+        var scaleY = translateCoords.scale[1];
+        scale = zoomListener.scale();
+        svgGroup.transition().attr("transform", "translate(" + translateX + "," + translateY + ")scale(" + scale + ")");
+        d3.select(domNode).select('g.node').attr("transform", "translate(" + translateX + "," + translateY + ")");
+        zoomListener.scale(zoomListener.scale());
+        zoomListener.translate([translateX, translateY]);
+        panTimer = setTimeout(function () {
+          pan(domNode, speed, direction);
+        }, 50);
+      }
+    }
 
   }]
 );
