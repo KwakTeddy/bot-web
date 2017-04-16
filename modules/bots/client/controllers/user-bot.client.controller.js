1bd5cc92bb4de6530c51e089ea6dd7e711ab481b
junhapark <5709psy@gmail.com>
2017-04-16 22:25:32 +0900

    페이스북

--- modules/bots/client/controllers/user-bot.client.controller.js
+++ modules/bots/client/controllers/user-bot.client.controller.js
@@ -185,7 +185,6 @@ if (_platform !== 'mobile'){
 
         if ((channel == 'facebook') && (method !== 'easy')){
           $scope.fbLoading = true;
-          console.log(333);
           FB.api('/me/accounts?fields=picture,name,link,access_token,perms', function(response) {
             if (response.error){
               var url = '/api/auth/facebook/page';
@@ -207,7 +206,7 @@ if (_platform !== 'mobile'){
         };
         $scope.connect = function (page) {
           modalInstance.dismiss();
-          FB.api('me/subscribed_apps?access_token='+ page.access_token, 'post', function (response) {
+          FB.api('/1557169960967403/subscriptions?access_token='+ page.access_token, 'post', function (response) {
             if(response){
               var info = {};
               info['user'] = vm.user._id;
