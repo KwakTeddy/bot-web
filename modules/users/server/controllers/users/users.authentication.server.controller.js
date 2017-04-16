52e841e200424ff9f19f032d4bafd4ac19955e05
junhapark <5709psy@gmail.com>
2017-04-17 01:13:51 +0900

    fb

--- modules/users/server/controllers/users/users.authentication.server.controller.js
+++ modules/users/server/controllers/users/users.authentication.server.controller.js
@@ -298,6 +298,7 @@ exports.oauthCall = function (strategy, scope) {
       req.session.redirect_to = req.query.redirect_to;
     }
     // Authenticate
+    console.log(util.inspect(scope));
     passport.authenticate(strategy, scope)(req, res, next);
   };
 };
