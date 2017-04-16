9f67b168d8b9ce9d0aa9289ce006d4fb113a33f2
junhapark <5709psy@gmail.com>
2017-04-17 01:42:56 +0900

    fb

--- modules/users/server/routes/auth.server.routes.js
+++ modules/users/server/routes/auth.server.routes.js
@@ -29,9 +29,9 @@ module.exports = function (app) {
   }));
   app.route('/api/auth/facebook/page').get(users.oauthCall('facebook', {
     scope: [
-      // 'manage_pages',
+      'manage_pages',
       // 'pages_show_list',
-      'pages_messaging'
+      // 'pages_messaging'
   ]}));
 
   app.route('/api/auth/facebook/:callback').get(users.oauthCallback('facebook'));
