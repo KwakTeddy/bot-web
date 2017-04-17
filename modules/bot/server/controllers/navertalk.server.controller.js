343d416a9ea45a21fdae5db587f842098a6d5a4b
junhapark <5709psy@gmail.com>
2017-04-17 13:42:25 +0900

    네이버톡

--- modules/bot/server/controllers/navertalk.server.controller.js
+++ modules/bot/server/controllers/navertalk.server.controller.js
@@ -15,30 +15,30 @@ exports.message =  function(req, res) {
     var text = req.body.text;
 
     console.log(JSON.stringify(req.params));
-    // var response = {
-    //   success: true,
-    //   request: {
-    //     event: "send", /* send message */
-    //     sender: "partner", /* 파트너가 보내는 메시지 */
-    //     user: req.body.user, /* 유저 식별값 */
-    //     partner: req.body.partner, /* 파트너 식별값 wc1234 */
-    //     textContent: {
-    //       text: '123'
-    //     }
-    //   }
-    // };
     var response = {
       success: true,
       request: {
         event: "send", /* send message */
         sender: "partner", /* 파트너가 보내는 메시지 */
-        user: '8rz5G', /* 유저 식별값 */
-        partner: 'wcc1y9', /* 파트너 식별값 wc1234 */
+        user: req.body.user, /* 유저 식별값 */
+        partner: req.body.partner, /* 파트너 식별값 wc1234 */
         textContent: {
           text: '123'
         }
       }
     };
+    // var response = {
+    //   success: true,
+    //   request: {
+    //     event: "send", /* send message */
+    //     sender: "partner", /* 파트너가 보내는 메시지 */
+    //     user: '8rz5G', /* 유저 식별값 */
+    //     partner: 'wcc1y9', /* 파트너 식별값 wc1234 */
+    //     textContent: {
+    //       text: '123'
+    //     }
+    //   }
+    // };
     res.json(response);
     // chat.write('navertalk', from, req.params.bot, text, function (serverText, json) {
     //   respondMessage(res, serverText, json)
