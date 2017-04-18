07424a237d48d6e8e284bb4ed0b40d0f65551b04
junhapark <5709psy@gmail.com>
2017-04-18 18:19:14 +0900

    페북

--- modules/core/server/routes/core.server.routes.js
+++ modules/core/server/routes/core.server.routes.js
@@ -7,6 +7,9 @@ module.exports = function (app) {
   // Define error pages
   app.route('/server-error').get(core.renderServerError);
 
+  // To Get Config
+  app.route('/config').get(core.getConfig);
+
   // Return a 404 for all undefined api, module or lib routes
   app.route('/:url(api|modules|lib)/*').get(core.renderNotFound);
 
