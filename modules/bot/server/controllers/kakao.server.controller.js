a48f78e4c6fd9f3513f0c2bbf1b61c15c57279ed
junhapark <5709psy@gmail.com>
2017-04-20 10:32:35 +0900

    페북

--- modules/bot/server/controllers/kakao.server.controller.js
+++ modules/bot/server/controllers/kakao.server.controller.js
@@ -3,7 +3,7 @@ var request = require('request');
 var path = require('path');
 var chat = require(path.resolve('modules/bot/server/controllers/bot.server.controller'));
 var contextModule = require(path.resolve('modules/bot/engine/common/context'));
-
+var util = require('util');
 exports.keyboard = function (req, res) {
   console.log("kakao keyboard");
 
