e716aecbd78f86f8167d459d6a997be50214c2b0
spikewj <spikewj@gmail.com>
2017-05-16 06:43:57 +0900

    edit 시작노드

--- custom_modules/sampletree2/default.graph.js
+++ custom_modules/sampletree2/default.graph.js
@@ -268,23 +268,25 @@ var dialogs = [
 ];
 
 var commonDialogs = [
-{
-  id: 'defaultcommon0',
-  filename: 'defaultcommon',
-  name: '시작',
-  input: '시작',
-  output: 'sampletree2 입니다.'
-},
-{
-  id: 'defaultcommon1',
-  filename: 'defaultcommon',
-  name: '답변없음',
-  input: '',
-  output: '알아듣지 못했습니다'
-}
+	{
+		"id": "defaultcommon0",
+		"filename": "defaultcommon",
+		"name": "시작",
+		"input": [
+			{
+				"text": "시작"
+			}
+		],
+		"output": "sampletree2 입니다. 왓???????"
+	},
+	{
+		"id": "defaultcommon1",
+		"filename": "defaultcommon",
+		"name": "답변없음",
+		"input": "",
+		"output": "알아듣지 못했습니다"
+	}
 ];
-
-
 var _bot = require(require('path').resolve("config/lib/bot")).getBot('sampletree2');
 _bot.setDialogs(dialogs);
 _bot.setCommonDialogs(commonDialogs);
