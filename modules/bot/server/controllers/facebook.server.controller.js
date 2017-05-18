9557ab541a5475d8f583c424d0da5317eb6013ae
junhapark <5709psy@gmail.com>
2017-05-18 18:43:03 +0900

    페이스북

--- modules/bot/server/controllers/facebook.server.controller.js
+++ modules/bot/server/controllers/facebook.server.controller.js
@@ -411,7 +411,7 @@ function sendButtonMessage(recipientId, text, task, token) {
     task.buttons[i].title = task.buttons[i].text;
     delete task.buttons[i].text;
 
-    if ( buttons[i].url){
+    if ( task.buttons[i].url){
       task.buttons[i]['type'] = 'web_url';
 
     }else {
