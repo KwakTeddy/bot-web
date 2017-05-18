da6b707602829179bffabc193dfbe38c62b681b5
junhapark <5709psy@gmail.com>
2017-05-18 18:08:50 +0900

    페이스북

--- modules/bot/server/controllers/facebook.server.controller.js
+++ modules/bot/server/controllers/facebook.server.controller.js
@@ -330,17 +330,25 @@ function receivedPostback(event) {
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
   var timeOfPostback = event.timestamp;
-
+  var token = '';
   // The 'payload' param is a developer-defined field which is set in a postback
   // button for Structured Messages.
   var payload = event.postback.payload;
+  UserBotFbPage.findOne({pageId: event.recipient.id}, function (err, data) {
+    if (err){
+      console.log(err)
+    }else {
+      token = data.accessToken;
 
-  console.log("Received postback for user %d and page %d with payload '%s' " +
-    "at %d", senderID, recipientID, payload, timeOfPostback);
+      console.log("Received postback for user %d and page %d with payload '%s' " +
+        "at %d", senderID, recipientID, payload, timeOfPostback);
 
-  // When a postback is called, we'll send a message back to the sender to
-  // let them know it was successful
-  sendTextMessage(senderID, "Postback called");
+      // When a postback is called, we'll send a message back to the sender to
+      // let them know it was successful
+      sendTextMessage(senderID, payload, task, token);
+    }
+  });
+
 }
 
 
