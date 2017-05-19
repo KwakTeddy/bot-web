f05d46c23883cf553df8368c19ca0b16b16ccb8c
Yongwon <dyddnjs0007@gmail.com>
2017-05-19 13:20:03 +0900

    MERGE

--- custom_modules/Shinhancard/Shinhancard.bot.js
+++ custom_modules/Shinhancard/Shinhancard.bot.js
@@ -2,7 +2,8 @@ var path = require('path');
 var botlib = require(path.resolve('config/lib/bot'));
 
 var Shinhancard = {
-  use: true
+  use: true,
+  topicKeywords: ['등록','가입','결제','비밀번호','패스워드','홍채','홍채인증','지문인증','지문','인증','앱카드','FAN','OTP','폰OTP','NFC']
 };
 
 botlib.makeBot('Shinhancard', Shinhancard);
--- custom_modules/Shinhancard/default.js
+++ custom_modules/Shinhancard/default.js
@@ -1,5 +1,49 @@
 var path = require('path');
 var bot = require(path.resolve('config/lib/bot')).getBot('Shinhancard');
+var mongoose = require('mongoose');
+var async = require('async');
+var mongo = require(path.resolve('./modules/bot/action/common/mongo'));
+var type = require(path.resolve('./modules/bot/action/common/type'));
+
+
+var fanfaqType = {
+    typeCheck: type.mongoDbTypeCheck,
+    limit: 5,
+    raw: true,
+    context: true,
+    mongo: {
+        model: 'fan_faq',
+        queryFields: ['title'],
+        fields: 'title content created' ,
+        taskFields: ['_id', 'title', 'content'],
+        taskSort: function(a, b) {
+            if(b.matchCount > a.matchCount) return 1;
+            else if(b.matchCount < a.matchCount) return -1;
+            else {
+                return 0;
+            }
+        },
+        //query: {},
+        // sort: "-created",
+        // limit: 5,
+        minMatch: 1,
+        checkRequired: function(text, type, inDoc, context) {
+            return '학습되어 있지 않은 질문 입니다.';
+        }
+    }
+}
+
+bot.setType('fanfaqType',fanfaqType);
+
+
+var faqSchema = {
+    title: {
+        type: String
+    },
+    content: {
+        type: String
+    }
+};
 
 var defaultTask = {
     name: 'defaultTask',
@@ -9,3 +53,50 @@ var defaultTask = {
 };
 bot.setTask("defaultTask", defaultTask);
 
+var te = {
+    name: 'te',
+    action: function (task,context,callback) {
+        //Converter Class
+        var Converter = require("csvtojson").Converter;
+
+        var fs=require("fs");
+//CSV File Path or CSV String or Readable Stream Object
+        var csvFileName="./fan_faq.csv";
+
+//new converter instance
+        var csvConverter=new Converter({});
+
+//end_parsed will be emitted once parsing finished
+        csvConverter.on("end_parsed",function(jsonObj){
+            var fan_faq = mongo.getModel('fan_faqs', faqSchema);
+            var docs = jsonObj;
+            var count =0;
+            var _doc = {
+                title: String,
+                content: String
+            };
+            console.log(jsonObj); //here is your result json object
+
+            if (docs) {
+                async.eachSeries(docs, function(doc, cb) {
+                    _doc.title = doc.Question;
+                    _doc.content = doc.Answer;
+                    fan_faq.update({title: _doc.title}, _doc, {upsert: true}, function (err) {
+                        if (err) {
+                            return err;
+                        }
+                    });
+                    count += 1;
+                    console.log('done: '+count);
+                    cb(null)
+                }, function (err) {
+                    callback(task,context);
+                });
+            }
+        });
+
+//read from file
+        fs.createReadStream(csvFileName).pipe(csvConverter);
+    }
+};
+bot.setTask("te", te);
