87616593c396b4195ee76c6541c7c0c603686e13
root <root@ip-172-31-14-53.ap-northeast-2.compute.internal>
2017-05-19 05:07:24 +0000

    shinhan

--- custom_modules/Shinhancard/Shinhancard.bot.js
+++ custom_modules/Shinhancard/Shinhancard.bot.js
@@ -3,7 +3,9 @@ var botlib = require(path.resolve('config/lib/bot'));
 
 var Shinhancard = {
   use: true,
-  topicKeywords: ['등록','가입','결제','비밀번호','패스워드','홍채','홍채인증','지문인증','지문','인증','앱카드','FAN','OTP','폰OTP','NFC']
+  kakao: {
+    keyboard: { type :"buttons", buttons:["시작"]}
+  }
 };
 
 botlib.makeBot('Shinhancard', Shinhancard);
