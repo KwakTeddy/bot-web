95c3542da1a687166f369ab30a09bfe85e0dddd3
Yongwon <dyddnjs0007@gmail.com>
2017-05-19 15:45:36 +0900

    MERGE

--- custom_modules/test/test.bot.js
+++ custom_modules/test/test.bot.js
@@ -1,14 +1,9 @@
 var path = require('path');
 var botlib = require(path.resolve('config/lib/bot'));
 
-var test = {
-  use: true,
-  serviceName: 'Coupang',
-  serviceNick: 'Coupang',
-  kakao: {
-    keyboard: { type :"buttons", buttons:["개발 테스트용입니다"]}
-  },
+var Test = {
+  use: true
 };
 
-botlib.makeBot('test', test);
+botlib.makeBot('Test', Test);
 
