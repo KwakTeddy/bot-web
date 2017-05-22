ab2c39cec89e0c83c9b06be5a429f5fce37c47e9
junhapark <5709psy@gmail.com>
2017-05-22 15:31:36 +0900

    관리자 channel

--- modules/bot/server/controllers/facebook.server.controller.js
+++ modules/bot/server/controllers/facebook.server.controller.js
@@ -73,6 +73,8 @@ exports.message = function (req, res) {
       // You must send back a 200, within 20 seconds, to let us know you've
       // successfully received the callback. Otherwise, the request will time out.
       res.sendStatus(200);
+  }else {
+    return false
   }
 };
 
