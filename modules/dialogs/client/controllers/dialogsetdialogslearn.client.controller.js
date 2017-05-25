5901432067c9cf1ba59b5131f087fb53b684dc5e
junhapark <5709psy@gmail.com>
2017-05-25 17:12:55 +0900

    개별 대화 학습

--- modules/dialogs/client/controllers/dialogsetdialogslearn.client.controller.js
+++ modules/dialogs/client/controllers/dialogsetdialogslearn.client.controller.js
@@ -44,6 +44,7 @@
     }
 
     $scope.$$postDigest(function () {
+      console.log('again?')
       if(vm.dialogs.length){
         for (var i = 0; i < vm.dialogs.length; i++){
           if(vm.dialogs[i].parent) {
@@ -51,8 +52,8 @@
               if(vm.dialogs[j]._id == vm.dialogs[i].parent){
                 // console.log('learnDialog_' + (i - 1))
                 // console.log(String(parseInt(document.getElementById('learnDialog_' + (i - 1)).style.width)*(vm.ratio))+ '%');
-                // document.getElementById('learnDialog_' + vm.dialogs[i]._id).style.width = String(parseInt(document.getElementById('learnDialog_' + vm.dialogs[j]._id).style.width)*(vm.ratio)) + '%'
-                document.getElementById('learnDialog_' + i).style.width = String(parseInt(document.getElementById('learnDialog_' + j).style.width)*(vm.ratio)) + '%'
+                document.getElementById('learnDialog_' + vm.dialogs[i]._id).style.width = String(parseInt(document.getElementById('learnDialog_' + vm.dialogs[j]._id).style.width)*(vm.ratio)) + '%'
+                // document.getElementById('learnDialog_' + i).style.width = String(parseInt(document.getElementById('learnDialog_' + j).style.width)*(vm.ratio)) + '%'
               }
             }
           }
@@ -90,12 +91,26 @@
 
 
     vm.createDepthDialog = function(parent, index) {
-      console.log(vm.childDialog)
       vm.childDialog['parent'] = parent._id;
       vm.childDialog.$save(function (response) {
-        console.log(response)
-        // vm.childDialog = response;
         vm.dialogs.splice(index+1, 0, response);
+
+        $scope.$$postDigest(function () {
+          for (var i = 0; i < vm.dialogs.length; i++){
+            if(vm.dialogs[i].parent) {
+              for(var j = 0; j < vm.dialogs.length; j++){
+                if(vm.dialogs[j]._id == vm.dialogs[i].parent){
+                  console.log(i + '/' + j)
+                  document.getElementById('learnDialog_' + vm.dialogs[i]._id).style.width = String(parseInt(document.getElementById('learnDialog_' + vm.dialogs[j]._id).style.width)*(vm.ratio)) + '%'
+                  // document.getElementById('learnDialog_' + i).style.width = String(parseInt(document.getElementById('learnDialog_' + j).style.width)*(vm.ratio)) + '%'
+                }
+              }
+            }
+          }
+        })
+
+
+
 
         // UserBotDialogService.query({botId: $cookies.get('default_bot')}, function (result) {
         //   vm.dialogs = result;
