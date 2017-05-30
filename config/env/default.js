2093024bfd6e5e7a60b83c0d580346f27ae7aad0
junhapark <5709psy@gmail.com>
2017-05-30 15:02:04 +0900

    개별사 이미지

--- config/env/default.js
+++ config/env/default.js
@@ -28,7 +28,7 @@ module.exports = {
   // for obsecurity reasons
   sessionKey: 'sessionId',
   sessionCollection: 'sessions',
-  logo: 'public/images/' + process.env.ENTERPRISE + '.png',
+  logo: 'public/images/' + process.env + '.png',
   favicon: 'modules/core/client/img/brand/playchat_favicon3.ico',
   uploads: {
     profileUpload: {
