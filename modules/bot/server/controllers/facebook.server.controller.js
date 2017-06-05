a12880e889706035d09817b55644f7ded2c2d884
junhapark <5709psy@gmail.com>
2017-06-05 18:08:55 +0900

    fb

--- modules/bot/server/controllers/facebook.server.controller.js
+++ modules/bot/server/controllers/facebook.server.controller.js
@@ -436,6 +436,10 @@ function sendTextMessage(recipientId, text, task, token) {
  *
  */
 function sendButtonMessage(recipientId, text, task, token) {
+  console.log(util.inspect(bot._currentDialog, {showHidden:false, depth: null}))
+  console.log(util.inspect(botContext.botUser._currentDialog, {showHidden:false, depth: null}))
+  console.log(util.inspect(botContext.bot.startDialog, {showHidden:false, depth: null}))
+  console.log('*************************************************************************&*&*&*&')
   if(bot && bot.commonButtons && bot.commonButtons.length && bot._currentDialog && (botContext.botUser._currentDialog.name != botContext.bot.startDialog.name)){
     for(var i = 0; i < bot.commonButtons.length; i++){
       task.buttons.pop();
