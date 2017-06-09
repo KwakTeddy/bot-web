c160d24221a9dcaf368303296a8132ba3126444c
Yongwon <dyddnjs0007@gmail.com>
2017-06-09 18:24:22 +0900

    shinhan

--- custom_modules/Shinhancard/Shinhancard.bot.js
+++ custom_modules/Shinhancard/Shinhancard.bot.js
@@ -11,8 +11,8 @@ var Shinhancard = {
     matchRate: 0.3, matchCount: 3,
     useList: true,
     listMatchRate: 0.7, listMatchCount: 7,
-    listOutput: '질문에 가장 유사한 답변을 찾았습니다.\n\n#typeDoc#+index+. +inputRaw+\n# 번호를 입력해 주세요1.',
-    contentOutput: '[+doc1.inputRaw+]\n+doc1.output+\n\n더 필요하신 게 있으시면 말씀해주세요1~\n'
+    listOutput: '질문에 가장 유사한 답변을 찾았습니다.\n\n#typeDoc#+index+. +inputRaw+\n# 번호를 입력해 주세요',
+    contentOutput: '[+doc1.inputRaw+]\n+doc1.output+\n\n더 필요하신 게 있으시면 말씀해주세요\n'
   }
 
 };
