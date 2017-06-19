6c73323107925f94d6490f4fc9da0f81ec62f17e
com2best <rltkd6tl!>
2017-06-19 15:51:46 +0900

    grid graph

--- custom_modules/test3/test3.bot.js
+++
@@ -1,9 +0,0 @@
-var path = require('path');
-var botlib = require(path.resolve('config/lib/bot'));
-
-var test3 = {
-  use: true
-};
-
-botlib.makeBot('test3', test3);
-
--- modules/bots/client/config/bots.client.routes.js
+++ modules/bots/client/config/bots.client.routes.js
@@ -100,6 +100,16 @@ angular.module('bots').config(['$stateProvider',
           fileResolve: readBotFile
         }
       })
+      .state('bots.dialog-graph3', {
+        url: '/dialog-graph3/:botId/:fileId',
+        templateUrl: 'modules/bots/client/views/dialog-graph3.client.view.html',
+        controller: 'DialogGraph3Controller',
+        controllerAs: 'vm',
+        resolve: {
+          botFilesResolve: getBotFiles,
+          fileResolve: readBotFile
+        }
+      })
       .state('bots.graph-knowledge', {
         url: '/graph-knowledge/:botId',
         templateUrl: 'modules/bots/client/views/graph-knowledge.client.view.html',
