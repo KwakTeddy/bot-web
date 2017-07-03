8f77fb2e59fc295d984cdde16d23dbc219909217
junhapark <5709psy@gmail.com>
2017-07-03 17:29:53 +0900

    fb

--- modules/bot/server/controllers/facebook.server.controller.js
+++ modules/bot/server/controllers/facebook.server.controller.js
@@ -859,6 +859,7 @@ function smartReplyMessage(recipientId, text, task, token) {
 function callSendAPI(messageData, PAGE_ACCESS_TOKEN, cb) {
   if(botContext && botContext.user && botContext.user.liveChat){
     if (botContext.user.liveChat > 1){
+      botContext.user.liveChat++;
       return true
     }
     botContext.user.liveChat++;
