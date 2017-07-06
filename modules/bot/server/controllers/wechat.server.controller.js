654f72554f024c8242b4d5a0c463dde7ff91a896
com2best <rltkd6tl!>
2017-07-06 14:41:23 +0900

    wechat apply

--- modules/bot/server/controllers/wechat.server.controller.js
+++ modules/bot/server/controllers/wechat.server.controller.js
@@ -1,6 +1,7 @@
 
 exports.message =  function(req, res) {
-  console.log(JSON.stringify(req));
+  console.log(JSON.stringify(req.params));
+  console.log(JSON.stringify(req.query));
 
   var signature = req.params['signature'];
   var timestamp = req.params['timestamp'];
