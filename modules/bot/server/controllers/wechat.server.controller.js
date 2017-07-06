1bdab2276e64ddfff8b04e4f6d993ebba125de76
com2best <rltkd6tl!>
2017-07-06 17:53:24 +0900

    wechat

--- modules/bot/server/controllers/wechat.server.controller.js
+++ modules/bot/server/controllers/wechat.server.controller.js
@@ -1,5 +1,8 @@
+var xml2json = require('xml2js');
+var path = require('path');
+var chat = require(path.resolve('modules/bot/server/controllers/bot.server.controller'));
 
-exports.message =  function(req, res) {
+exports.messageGet = function(req, res) {
   var signature = req.query['signature'];
   var timestamp = req.query['timestamp'];
   var nonce = req.query['nonce'];
@@ -12,4 +15,41 @@ exports.message =  function(req, res) {
 
   res.write(echostr);
   res.end();
+}
+
+exports.message =  function(req, res) {
+  var body = '';
+  req.on('data', function (data) {
+    body += data;
+    if (body.length > 1e6)
+      req.connection.destroy();
+  });
+
+  req.on('end', function () {
+    console.log(body);
+
+    var result;
+    try {
+      xml2json.parseString(body, {explicitArray: false}, function(err, js) {
+        receivedMessage(res, js);
+      });
+    } catch (exception) {
+      res.write('');
+      res.end();
+    }
+  });
+}
+
+
+function receivedMesage(req, res, json) {
+  var from = json.FromUserName;
+  var text = json.Content;
+
+  chat.write('wechat', from, req.params.bot, text, json, function (serverText, json) {
+    respondMessage(res, serverText, json)
+  });
+}
+
+function respondMessage(res, text, json) {
+
 }
