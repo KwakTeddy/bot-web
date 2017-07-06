d89388bfedb88c77530f3ac173c688c58da8b258
junhapark <5709psy@gmail.com>
2017-07-06 14:15:51 +0900

    test

--- modules/bot/engine/common/context.js
+++ modules/bot/engine/common/context.js
@@ -30,7 +30,7 @@ function getContext(botName, channel, user, options, callback) {
         var _user = {userId: user, channel: channel, bot: botName};
         botUser.getUserContext(_user, null, function (_user, _context) {
           userContext = {userId: user, channel: channel, bot: botName};
-          console.log(util.inspect('~~~~~~~~~~~~~~~~~~~~@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@' + global._users[user]));
+          console.log('~~~~~~~~~~~~~~~~~~~~@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@' + util.inspect(global._users[user]));
           if(global._users[user] && global._users[user].liveChat) userContext['liveChat'] = global._users[user].liveChat;
           userContext = utils.merge(userContext, _user.doc._doc);
 
