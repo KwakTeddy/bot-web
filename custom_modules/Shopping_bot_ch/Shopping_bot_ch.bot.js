fd2ab947604b23a764a736b076b587b33edcdf7b
tanpang <tanpang1@gmail.com>
2017-07-10 12:16:22 +0900

    shpping ch fix1

--- custom_modules/Shopping_bot_ch/Shopping_bot_ch.bot.js
+++
@@ -1,9 +0,0 @@
-var path = require('path');
-var botlib = require(path.resolve('config/lib/bot'));
-
-var Shopping_bot_ch = {
-  use: true
-};
-
-botlib.makeBot('Shopping_bot_ch', Shopping_bot_ch);
-
