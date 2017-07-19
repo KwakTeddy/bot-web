304e18f76ddf81d1b7484c49d12473f88823fecd
tanpang <tanpang1@gmail.com>
2017-07-19 17:42:12 +0900

    logprint fix

--- modules/bot/server/controllers/bot.server.controller.js
+++ modules/bot/server/controllers/bot.server.controller.js
@@ -36,8 +36,8 @@ exports.setBotSocket = function(socket) {botSocket = socket};
 
 // console = {};
 console.log = function(out) {
-  // process.stdout.write(out+'\n');
-  // if(botSocket) botSocket.emit('send_msg', ":log \n" + out +"\n");
+  process.stdout.write(out+'\n');
+  if(botSocket) botSocket.emit('send_msg', ":log \n" + out +"\n");
 }
 
 console.error = function(out) {
