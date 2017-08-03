40477915580920f5719e7d3376e176ad982a7598
junhapark <5709psy@gmail.com>
2017-08-03 13:24:23 +0900

    bower innerhtml on svg

--- config/assets/default.js
+++ config/assets/default.js
@@ -37,6 +37,7 @@ module.exports = {
         'public/lib/pnotify/dist/pnotify.js',
         'public/lib/angular-pnotify/src/angular-pnotify.js',
         'public/lib/ment.io/dist/mentio.js',
+        'public/lib/innersvg-polyfill/innersvg.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
