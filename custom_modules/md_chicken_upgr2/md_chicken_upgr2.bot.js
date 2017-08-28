e93220b4f2d2a258aa251da0fbb8d195f306967a
tanpang <tanpang1@gmail.com>
2017-08-28 18:52:49 +0900

    chicken start button

--- custom_modules/md_chicken_upgr2/md_chicken_upgr2.bot.js
+++ custom_modules/md_chicken_upgr2/md_chicken_upgr2.bot.js
@@ -2,7 +2,10 @@ var path = require('path');
 var botlib = require(path.resolve('config/lib/bot'));
 
 var md_chicken_upgr2 = {
-  use: true
+    use: true,
+    kakao: {
+        keyboard: { type :"buttons", buttons:["배달주문 시작!"]}
+    },
 };
 
 botlib.makeBot('md_chicken_upgr2', md_chicken_upgr2);
