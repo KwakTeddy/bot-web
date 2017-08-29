c850cb5a9572f1fca9c9f84d0a444c6119dbedd5
junhapark <5709psy@gmail.com>
2017-08-29 14:11:33 +0900

    lodingScreen

--- modules/bot-auths/server/routes/bot-auths.server.routes.js
+++ modules/bot-auths/server/routes/bot-auths.server.routes.js
@@ -8,14 +8,14 @@ var botAuthsPolicy = require('../policies/bot-auths.server.policy'),
 
 module.exports = function(app) {
   // Bot auths Routes
-  app.route('/api/bot-auths').all(botAuthsPolicy.isAllowed)
+  app.route('/api/bot-auths')//.all(botAuthsPolicy.isAllowed)
     .get(botAuths.list)
     .post(botAuths.create);
 
-  app.route('/api/bot-auths/getAuth').all(botAuthsPolicy.isAllowed)
+  app.route('/api/bot-auths/getAuth')//.all(botAuthsPolicy.isAllowed)
     .post(botAuths.getAuth);
 
-  app.route('/api/bot-auths/:botId/:userId').all(botAuthsPolicy.isAllowed)
+  app.route('/api/bot-auths/:botId/:userId')//.all(botAuthsPolicy.isAllowed)
     .get(botAuths.read)
     .put(botAuths.update)
     .delete(botAuths.delete);
