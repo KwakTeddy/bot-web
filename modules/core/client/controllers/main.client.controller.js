6399f2e465c677c3d619665be9610621d37f5fc0
rayim <rayim@moneybrain.ai>
2017-09-27 17:57:13 +0900

    [Ray] Loading 화면 hide/show animation 적용.

--- modules/core/client/controllers/main.client.controller.js
+++ modules/core/client/controllers/main.client.controller.js
@@ -2,12 +2,30 @@
 
 //플레이챗 전반적인 관리
 
-angular.module('core').controller('MainController', ['$scope', 'Authentication', '$location', function ($scope, Authentication, $location)
+angular.module('core').controller('MainController', ['$scope', 'Authentication', '$location', '$timeout', function ($scope, Authentication, $location, $timeout)
 {
     console.log('로그인 : ', Authentication.user);
 
-    //로딩 페이지 사라지는 애니메이션.
     $scope.loading = true;
+
+    $scope.$watch('loading', function(after, before)
+    {
+        if(!after)
+        {
+            angular.element('.main-logo-background').css('opacity', 0);
+            $timeout(function()
+            {
+                angular.element('.main-logo-background').css('display', 'none');
+            }, 2100);
+        }
+        else if(!before && after)
+        {
+            angular.element('.main-logo-background').css('display', '');
+            $timeout(function(){
+                angular.element('.main-logo-background').css('opacity', 1);
+            }, 100);
+        }
+    });
 
     if(!Authentication.user)
     {
