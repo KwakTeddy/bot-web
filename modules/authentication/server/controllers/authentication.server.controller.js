fa158eb732ff1e7d7281bb9e2fa7bc1eda69a59c
rayim <rayim@moneybrain.ai>
2017-11-02 10:58:04 +0900

    [Ray] authentication 로직 수정

--- modules/authentication/server/controllers/authentication.server.controller.js
+++ modules/authentication/server/controllers/authentication.server.controller.js
@@ -32,8 +32,6 @@ module.exports.signin = function(req, res, next)
 
 module.exports.signout = function (req, res)
 {
-    console.log('머야 : ');
-
     req.logout();
     req.session.destroy();
     res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -2,9 +2,9 @@
 
 //플레이챗 전반적인 관리
 
-angular.module('playchat').controller('HeaderController', ['$scope', '$location', '$timeout', '$cookies', 'Authentication', function ($scope, $location, $timeout, $cookies, Authentication)
+angular.module('playchat').controller('HeaderController', ['$scope', '$location', '$timeout', '$cookies', '$window', 'Authentication', function ($scope, $location, $timeout, $cookies, $window, Authentication)
 {
-    $scope.isLogin = $cookies.get('login');
+    $scope.isLogin = $cookies.get('login') == 'true';
 
     angular.element('.user-menu a').on('click', function()
     {
@@ -18,10 +18,13 @@ angular.module('playchat').controller('HeaderController', ['$scope', '$location'
 
         x -= angular.element('.user-menu').css('width').replace('px', '')*1;
 
-
         console.log(angular.element('.user-menu'));
 
-
         angular.element('.user-menu').css('left', x + 'px').css('top', y + 'px').show();
+    };
+
+    $scope.signout = function()
+    {
+        $window.location.href = '/api/auth/signout';
     };
 }]);
