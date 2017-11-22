571e25e760aa0cab7a3c58459df01926948023d4
rayim <rayim@moneybrain.ai>
2017-11-22 14:19:09 +0900

    다이얼로그 그래프 패스 화면 작업.

--- modules/playchat/working-ground/dialog-graph-path/client/controllers/dialog-graph-path-analysis.client.controller.js
+++ modules/playchat/working-ground/dialog-graph-path/client/controllers/dialog-graph-path-analysis.client.controller.js
@@ -32,6 +32,20 @@ angular.module("playchat").controller("DialogGraphPathAnalysisController", ['$sc
             return item.dialogName;
         };
 
+        var createLine = function(x1, y1, x2, y2)
+        {
+            var line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+            line.setAttribute('x1', x1);
+            line.setAttribute('y1', y1);
+            line.setAttribute('x2', x2);
+            line.setAttribute('y2', y2);
+            line.setAttribute('stroke', '#b1dbf4');
+            line.setAttribute('stroke-width', '1px');
+            line.setAttribute('shape-rendering', 'crispEdges');
+
+            return line;
+        };
+
         $scope.getList = function()
         {
             DialogGraphPathService.query({ botId: chatbot.id, startDate: new Date($scope.date.start).toISOString(), endDate: new Date($scope.date.end).toISOString() }, function(list)
@@ -53,11 +67,48 @@ angular.module("playchat").controller("DialogGraphPathAnalysisController", ['$sc
                 $scope.paths = [];
                 for(var key in dialogMap)
                 {
-                    var list = [];
                     var count = dialogMap[key];
-
                     $scope.paths.push({ list: key.split('-'), count: count });
                 }
+
+                setTimeout(function()
+                {
+                    var svg = angular.element('#lineSvg');
+                    angular.element('.dialog-graph-path-card').each(function()
+                    {
+                        var left = this.offsetLeft;
+                        var top = this.offsetTop;
+
+                        var halfOfHeight = this.offsetHeight / 2;
+
+                        var prev = this.previousElementSibling;
+                        if(prev)
+                        {
+                            var prevLeft = prev.offsetLeft;
+                            var prevRight = prev.offsetLeft + prev.offsetWidth;
+                            var prevTop = prev.offsetTop;
+                            var prevHalfOfHeight = prev.offsetHeight / 2;
+
+                            if(prevRight < left)
+                            {
+                                svg.append(createLine(prevRight, prevTop + prevHalfOfHeight, left, top + halfOfHeight));
+                            }
+                            else if(prevTop + prevHalfOfHeight < top + halfOfHeight)
+                            {
+                                var halfOfWidth = this.offsetWidth / 2;
+
+                                var prevBottom = prev.offsetTop + prev.offsetHeight;
+                                var prevHalfOfWidth = prev.offsetWidth / 2;
+
+                                svg.append(createLine(prevLeft + prevHalfOfWidth, prevBottom, prevLeft + prevHalfOfWidth, prevBottom + 10));
+                                svg.append(createLine(left + halfOfWidth, top - 10, prevLeft + prevHalfOfWidth, prevBottom + 10));
+                                svg.append(createLine(left + halfOfWidth, top - 10, left + halfOfWidth, top));
+                            }
+                        }
+
+                        console.log(left, top);
+                    });
+                }, 100);
 
                 $scope.$parent.loaded('working-ground');
             },
