7ca597940c98a8415641ff9c1a1391eb48b8daf3
rayim <rayim@moneybrain.ai>
2017-12-05 15:18:46 +0900

    [Ray] 버그수정

--- modules/core/client/services/language.client.service.js
+++ modules/core/client/services/language.client.service.js
@@ -288,7 +288,7 @@
                 "jp": "書く",
                 "zh": "写"
             },
-            "\"Flow chatbots are tree-based. The user is driven down a specific path": {
+            "Flow chatbots are tree-based. The user is driven down a specific path": {
                 "ko": "플로우 챗봇은 트리구조입니다. 사용자는 개발자가 지정한 특정 대화 구조에 맞게 대화를 진행하게 됩니다.",
                 "en": "\"Flow chatbots are tree-based. The user is driven down a specific path",
                 "jp": " a path previously defined by the chatbot's developer\"",
@@ -1533,6 +1533,12 @@
             "Signout": {
                 "ko": "로그아웃",
                 "en": "Signout"
+            },
+            "ID": {
+                "ko": "아이디",
+                "en": "Id",
+                "zh" : "Id",
+                "jp" : "Id"
             }
         };
 
@@ -1545,6 +1551,8 @@
             {
                 if(!list[key])
                     list[key] = true;
+
+                console.error('language', key);
 
                 return '언어가 없습니다';
             }
