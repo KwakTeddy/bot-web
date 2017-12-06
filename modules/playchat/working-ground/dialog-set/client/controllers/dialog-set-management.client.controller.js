c75c022346c0844b20e6317256d424c4f8f82749
rayim <rayim@moneybrain.ai>
2017-12-06 18:24:20 +0900

    [Ray] 버그수정 및 개선

--- modules/playchat/working-ground/dialog-set/client/controllers/dialog-set-management.client.controller.js
+++ modules/playchat/working-ground/dialog-set/client/controllers/dialog-set-management.client.controller.js
@@ -141,7 +141,6 @@ angular.module('playchat').controller('DialogSetManagementController', ['$window
 
         $scope.delete = function(item)
         {
-            console.log(item);
             if(confirm($scope.lan('Are you sure you want to delete this item?')))
             {
                 var params = {};
