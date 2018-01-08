e651ec008386e4b61e5193b97cf40f9ce2d5f211
rayim <rayim@moneybrain.ai>
2018-01-08 16:11:49 +0900

    [Ray][PC-139] 로드밸런스 개선

--- engine/bot/server/controllers/rest.server.controller.js
+++ engine/bot/server/controllers/rest.server.controller.js
@@ -1,29 +1,42 @@
 var path = require('path');
+var engine = require(path.resolve('engine/bot/server/controllers/bot.server.controllers.js'));
 var master = require(path.resolve('engine/loadbalancer/master.js'));
+
+var isMaster = process.env.isMaster;
+
+var callback = function (serverText, json)
+{
+    console.log('왔습니다');
+    if(json == undefined || (json.result == undefined && json.image == undefined && json.buttons == undefined && json.items == undefined)) {
+        res.write(serverText);
+        res.end();
+    } else if(json.result) {
+        if(json.result.text == undefined) json.result.text = serverText;
+        res.write(JSON.stringify(json.result));
+        res.end();
+    } else {
+        json.text = serverText;
+        json.topTask = undefined;
+        res.write(JSON.stringify(json));
+        res.end();
+    }
+};
 
 exports.message = function (req, res)
 {
-    console.log(JSON.stringify(req.body));
+    console.log('여기지? : ', JSON.stringify(req.body));
 
     var msg = req.body;
     try
     {
-        master.routing(msg.channel, msg.user, msg.bot, msg.text, msg, function (serverText, json)
+        if(isMaster)
         {
-            if(json == undefined || (json.result == undefined && json.image == undefined && json.buttons == undefined && json.items == undefined)) {
-                res.write(serverText);
-                res.end();
-            } else if(json.result) {
-                if(json.result.text == undefined) json.result.text = serverText;
-                res.write(JSON.stringify(json.result));
-                res.end();
-            } else {
-                json.text = serverText;
-                json.topTask = undefined;
-                res.write(JSON.stringify(json));
-                res.end();
-            }
-        });
+            master.routing(msg.channel, msg.user, msg.bot, msg.text, msg, callback);
+        }
+        else
+        {
+            engine.write(msg.channel, msg.user, msg.bot, msg.text, msg, callback);
+        }
     }
     catch(e)
     {
