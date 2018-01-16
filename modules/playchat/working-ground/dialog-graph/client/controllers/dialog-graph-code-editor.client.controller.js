dc6f13303d93027f3bd10df810413f12781b90b3
rayim <rayim@moneybrain.ai>
2018-01-16 11:41:39 +0900

    [Ray][PC-295] 코드에디터 탭 별로 에디터 인스턴스 유지.

--- modules/playchat/working-ground/dialog-graph/client/controllers/dialog-graph-code-editor.client.controller.js
+++ modules/playchat/working-ground/dialog-graph/client/controllers/dialog-graph-code-editor.client.controller.js
@@ -6,11 +6,7 @@
 
         var chatbot = $cookies.getObject('chatbot');
 
-        var editor = CodeMirror.fromTextArea($element.find('textarea').get(0),
-        {
-            lineNumbers: true,
-            smartIndent: true
-        });
+        var editors = {};
 
         $scope.myBotAuth = chatbot.myBotAuth;
         $scope.currentFileName = undefined;
@@ -19,16 +15,14 @@
 
         var fileName = $location.search().fileName;
 
+        var editor = undefined;
+
         (function()
         {
             window.addEventListener('keydown', function(e)
             {
                 if(location.href.indexOf('/playchat/development/dialog-graph') == -1 || angular.element('.dialog-graph-code-editor').is(':visible') == false)
                     return;
-
-                console.log(e.keyCode);
-
-                console.log(e);
 
                 if(e.path[0].nodeName == 'TEXTAREA')
                 {
@@ -47,36 +41,11 @@
                         return;
                     }
 
-
-                    if(e.altKey && e.keyCode == 37)
-                    {
-                        console.log(DialogGraph.$scope.currentTabName.replace(/\./gi, '\\\\.'));
-
-                        var prev = angular.element('#' + DialogGraph.$scope.currentTabName.replace(/\./gi, '\\.')).prev();
-                        var id = prev.attr('id').replace(/\./gi, '\\\\.');
-
-                        DialogGraph.$scope.selectTab({ currentTarget: '#' + id}, prev.attr('id'));
-                        DialogGraph.$scope.currentTabName = prev.attr('id');
-                    }
-                    else if(e.altKey && e.keyCode == 39)
-                    {
-                        var next = angular.element('#' + DialogGraph.$scope.currentTabName.replace(/\./gi, '\\.')).next();
-                        if(next.length == 1)
-                        {
-                            var id = next.attr('id').replace(/\./gi, '\\\\.');
-                            DialogGraph.$scope.selectTab({ currentTarget: '#' + id}, next.attr('id'));
-                            DialogGraph.$scope.currentTabName = next.attr('id');
-                        }
-                    }
-                    else if(e.keyCode == 83 && (e.ctrlKey || e.metaKey))
+                    if(e.keyCode == 83 && (e.ctrlKey || e.metaKey))
                     {
                         $scope.save();
                         e.preventDefault();
                         e.stopPropagation();
-                    }
-                    else
-                    {
-                        console.log(e.keyCode);
                     }
                 }
             });
@@ -87,10 +56,81 @@
             });
         })();
 
-
         function openCodeEditor(fileName, options)
         {
             $scope.mode = '';
+
+            angular.element('.dialog-graph-code-editor-body textarea+.CodeMirror').hide();
+
+            editor = editors[fileName];
+            if(!editor)
+            {
+                angular.element('.dialog-graph-code-editor-body').prepend('<textarea style="display: none;"></textarea>');
+                editors[fileName] = editor = CodeMirror.fromTextArea($element.find('textarea:first').get(0),
+                {
+                    lineNumbers: true,
+                    smartIndent: true,
+                    autofocus: true
+                });
+
+                DialogGraphsService.get({ botId: chatbot.id, fileName: fileName}, function(result)
+                {
+                    angular.element('.dialog-graph-code-editor').show();
+
+                    if(options)
+                    {
+                        if(options.isView)
+                        {
+                            editor.setValue(result.data);
+                            editor.focus();
+
+                            var split = result.data.split('\n');
+                            for(var i=0; i<split.length; i++)
+                            {
+                                if(split[i].indexOf('var ' + options.target) != -1)
+                                {
+                                    editor.setCursor({line: i, ch: 1});
+                                }
+                            }
+                        }
+                        else if(options.isCreate)
+                        {
+                            angular.element('.dialog-graph-code-editor-controller').addClass('edit');
+                            editor.setValue(result.data + '\n\n' + options.code);
+                            editor.focus();
+                            editor.setCursor({line: result.data.split('\n').length + 1, ch: 1});
+                        }
+                        else if(options.mode == 'graphsource')
+                        {
+                            $scope.mode = options.mode;
+                            editor.setValue(result.data);
+                        }
+                    }
+                    else
+                    {
+                        editor.setValue(result.data);
+                    }
+
+                    editor.focus();
+
+                    $scope.currentFileName = fileName;
+                },
+                function(err)
+                {
+                    alert(err.data.message);
+                });
+            }
+            else
+            {
+                $scope.currentFileName = fileName;
+                angular.element(editor.getTextArea()).next().show();
+                angular.element('.dialog-graph-code-editor').show();
+
+                var cursor = editor.getCursor();
+                console.log('커서 : ', cursor);
+                editor.focus();
+                editor.setCursor({line: cursor.line, ch: cursor.ch});
+            }
 
             angular.element('.dialog-graph-code-editor-controller').removeClass('edit');
 
@@ -98,53 +138,6 @@
             {
                 angular.element('.dialog-graph-code-editor-controller').addClass('edit');
             }
-
-            DialogGraphsService.get({ botId: chatbot.id, fileName: fileName}, function(result)
-            {
-                angular.element('.dialog-graph-code-editor').show();
-
-                if(options)
-                {
-                    if(options.isView)
-                    {
-                        editor.setValue(result.data);
-                        editor.focus();
-
-                        var split = result.data.split('\n');
-                        for(var i=0; i<split.length; i++)
-                        {
-                            if(split[i].indexOf('var ' + options.target) != -1)
-                            {
-                                editor.setCursor({line: i, ch: 1});
-                            }
-                        }
-                    }
-                    else if(options.isCreate)
-                    {
-                        angular.element('.dialog-graph-code-editor-controller').addClass('edit');
-                        editor.setValue(result.data + '\n\n' + options.code);
-                        editor.focus();
-                        editor.setCursor({line: result.data.split('\n').length + 1, ch: 1});
-                    }
-                    else if(options.mode == 'graphsource')
-                    {
-                        $scope.mode = options.mode;
-                        editor.setValue(result.data);
-                    }
-                }
-                else
-                {
-                    editor.setValue(result.data);
-                }
-
-                editor.focus();
-
-                $scope.currentFileName = fileName;
-            },
-            function(err)
-            {
-                alert(err.data.message);
-            });
         };
 
         if(fileName && fileName.endsWith('.js') && !fileName.endsWith('.graph.js'))
