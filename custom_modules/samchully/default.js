3b600662ec9b8be0887811a07895e86dbcb1e750
rayim <rayim@moneybrain.ai>
2018-02-01 15:39:41 +0900

    [Ray] 삼천리 오류 수정

--- custom_modules/samchully/default.js
+++ custom_modules/samchully/default.js
@@ -138,6 +138,10 @@ module.exports = function(bot)
                     }
                 });
             }
+            else
+            {
+                callback();
+            }
         }
     });
 
