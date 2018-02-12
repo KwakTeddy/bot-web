018d8ee8f4cba03be4bcb62ecb367f33005e88b4
zsslovelyg <zsslovelyg@moneybrain.ai>
2018-02-12 09:53:23 +0900

    [Rita] 꽃배달봇 자체봇 개발

--- custom_modules/flower_moneybrain/flower_moneybrain.bot.js
+++ custom_modules/flower_moneybrain/flower_moneybrain.bot.js
@@ -1,14 +1,14 @@
 module.exports = function(options)
 {
     options.use = true;
-    // options.naver= {
-    //     clientId: 'Aqi_RlMlLRlJnmJptMhD',
-    //         clientSecret: '0AKq2NoNgn'
-    // };
-    // options.kakao={
-    //     keyboard: {
-    //         type:"buttons",buttons:["대화 시작"]
-    //     }
-    // };
+    options.naver= {
+        clientId: 'Aqi_RlMlLRlJnmJptMhD',
+            clientSecret: '0AKq2NoNgn'
+    };
+    options.kakao={
+        keyboard: {
+            type:"buttons",buttons:["대화 시작"]
+        }
+    };
 };
 
