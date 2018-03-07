4c9b823a40a069f79db728be9cac792b10eb0e69
zsslovelyg <zsslovelyg@moneybrain.ai>
2018-03-07 10:20:57 +0900

    [Rita] 포에버성형외과봇 wells 수정 kakao 프라스친구 추가

--- custom_modules/wells/wells.bot.js
+++ custom_modules/wells/wells.bot.js
@@ -2,4 +2,9 @@ module.exports = function(options)
 {
     options.version = 1.0;
     options.use = true;
+    options.kakao={
+        keyboard: {
+            type:"buttons",buttons:["대화 시작"]
+        }
+    };
 };
