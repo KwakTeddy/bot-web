630d97e42330484aa063eccf78d61d181879a693
rayim <rayim@moneybrain.ai>
2018-03-13 04:25:54 +0900

    [Ray] 오류 수정

--- modules/chatbots/client/controllers/chatbot.client.controller.js
+++ modules/chatbots/client/controllers/chatbot.client.controller.js
@@ -87,7 +87,11 @@
                     if(!version)
                     {
                         alert(LanguageService('It is a chatbot made in old version PlayChat. Go to old version PlayChat.'));
-                        location.href = 'https://old.playchat.ai/playchat/chatbots';
+                        console.log('흠 : ', 'https://old.playchat.ai/playchat/chatbots');
+                        window.open(
+                            'https://old.playchat.ai/playchat/chatbots',
+                            '_blank' // <- This is what makes it open in a new window.
+                        );
                     }
                     else
                     {
