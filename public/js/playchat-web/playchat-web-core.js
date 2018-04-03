05d55c477f29104b76704d4579678bc9010724c4
rayim <rayim@moneybrain.ai>
2018-04-03 14:37:29 +0900

    [Ray] 미니 웹 채팅 구현

---
+++ public/js/playchat-web/playchat-web-core.js
@@ -0,0 +1,194 @@
+(function()
+{
+    var Socket = function()
+    {
+        this.host = 'http://localhost:8443';
+        this.userId = new Date().getTime();
+        this.botId = document.body.getAttribute('data-id');
+        this.botName = document.body.getAttribute('data-name');
+
+        this.container = document.body.querySelector('.PlayChatBodyContainer');
+
+        if(!this.botId)
+        {
+            return console.error('botId is not defined');
+        }
+
+        this.init();
+    };
+
+    Socket.prototype.send = function(msg)
+    {
+        var params = {};
+        params.bot = this.botId;
+        params.user = this.userId;
+        params.msg = msg;
+
+        this.socket.emit('send_msg', params);
+    };
+
+    var getCurrentTime = function(time)
+    {
+        var date = undefined;
+        if(time)
+            date = new Date(time);
+        else
+            date = new Date();
+
+        var options = { weekday: 'long', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+        return date.toLocaleDateString('ko-KR', options);
+    };
+
+    Socket.prototype.receive = function(data)
+    {
+        var that = this;
+
+        var output = data.output;
+
+        var template = '<div class="answer">\n' +
+                       '    <img src="' + this.host + '/modules/playchat/simulator/client/imgs/bot.png">\n' +
+                       '    <div class="system-text">\n' +
+                       '        <span class="name">{botName}</span>\n' +
+                       '        <span class="time">{time}</span>\n' +
+                       '    </div>\n' +
+                       '    <div class="bubble">\n' +
+                       '        <em class="tail"></em>\n' +
+                       '        <div class="speech">\n' +
+                       '            <div class="speech-text">{text}</div>\n' +
+                       '        </div>\n' +
+                       '    </div>\n' +
+                       '</div>';
+
+        template = template.replace('{botName}', this.botName).replace('{time}', getCurrentTime()).replace('{text}', (output.text || '' ).replace(/</gi, '&lt;').replace(/>/gi, '&gt;').replace(/\n/gi, '<br>'));
+
+        var div = document.createElement('div');
+        div.innerHTML = template;
+
+        template = div.children[0];
+
+        if(output.image && output.image.url)
+        {
+            var t = '<div class="output-image">';
+            t += '<img src="' + output.image.url + '" alt="' + output.image.displayname + '">';
+            t += '</div>';
+
+            template.querySelector('.speech').innerHTML = t;
+        }
+
+        if(output.buttons)
+        {
+            var t = '';
+
+            for(var i=0; i<output.buttons.length; i++)
+            {
+                if(output.buttons[i].url)
+                {
+                    t = '<a href="' + output.buttons[i].url + '" style="color: #038eda; border: 0;" target="_blank">' + output.buttons[i].text + '</a>' + t;
+                }
+                else
+                {
+                    t += '<a style="cursor: pointer;" href="#" target="_blank">' + output.buttons[i].text + '</a>';
+                }
+            }
+
+            t += '</div>';
+
+            template.querySelector('.speech').innerHTML = '<div class="output-buttons">' + t;
+        }
+
+        this.container.appendChild(template);
+
+        var buttons = this.container.querySelectorAll('.output-buttons a');
+        for(var i=0; i<buttons.length; i++)
+        {
+            buttons[i].addEventListener('click', function(e)
+            {
+                var output = this.innerText;
+                that.send(output);
+
+                e.preventDefault();
+                e.stopPropagation();
+            });
+        }
+
+        this.container.scrollTop = this.container.scrollHeight;
+    };
+
+    Socket.prototype.addUserBubble = function(text)
+    {
+        var template = '<div class="question">\n' +
+                       '    <div class="system-text">\n' +
+                       '        <span class="time">{time}</span>\n' +
+                       '    </div>\n' +
+                       '    <div class="bubble">\n' +
+                       '        <em class="tail"></em>\n' +
+                       '        <div>\n' +
+                       '            <div class="speech">\n' +
+                       '                <div class="speech-text">{text}</div>\n' +
+                       '            </div>\n' +
+                       '        </div>\n' +
+                       '    </div>\n' +
+                       '</div>';
+
+        template = template.replace('{time}', getCurrentTime()).replace('{text}', text);
+
+        var div = document.createElement('div');
+        div.innerHTML = template;
+
+        template = div.children[0];
+
+        this.container.appendChild(template);
+
+        this.container.scrollTop = this.container.scrollHeight;
+    };
+
+    Socket.prototype.init = function()
+    {
+        var that = this;
+
+        var host = 'http://localhost:8443';
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = host + '/socket.io/socket.io.js';
+        script.onload = function()
+        {
+            var socket = io(host);
+            socket.on('connect', function()
+            {
+                that.socket = socket;
+                that.send(':reset user');
+
+                document.body.querySelector('.PlayChatMsgContainer input').addEventListener('keydown', function(e)
+                {
+                    if(e.keyCode == 13)
+                    {
+                        var text = this.value;
+                        that.addUserBubble(text);
+                        this.value = '';
+                        that.send(text);
+                    }
+                });
+            });
+
+            socket.on('send_msg', function(data)
+            {
+                that.receive(data);
+            });
+
+            socket.on('disconnect', function()
+            {
+                var div = document.createElement('div');
+                div.innerText = '연결이 종료되었습니다';
+                div.style.fontSize = '12px';
+                div.style.color = '#aaa';
+                div.style.textAlign = 'center';
+
+                that.container.appendChild(div);
+            });
+        };
+
+        document.body.appendChild(script);
+    };
+
+    new Socket();
+})();
---
+++ public/js/playchat-web/playchat-web.js
@@ -0,0 +1,74 @@
+(function()
+{
+    var PlayChatWeb = function(botId, botName)
+    {
+        this.host = 'http://localhost:8443';
+        this.botId = botId;
+        this.botName = botName;
+
+        this.initElements();
+    };
+
+    (function()
+    {
+        this.initElements = function()
+        {
+            this.iframe = document.createElement('iframe');
+            this.iframe.style.position = 'fixed';
+            this.iframe.style.right = '20px';
+            this.iframe.style.bottom = '20px';
+            this.iframe.style.width = '300px';
+            this.iframe.style.height = '400px';
+            this.iframe.style.border = '1px solid #ddd';
+            this.iframe.style.boxShadow = '0px 4px 10px 0px rgba(0, 0, 0, 0.75)';
+            this.iframe.style.zIndex = 1000;
+
+            document.body.appendChild(this.iframe);
+
+            this.container = document.createElement('div');
+            this.container.className = 'PlayChatWebContainer';
+            this.container.style.display = 'none';
+
+            this.header = document.createElement('div');
+            this.header.className = 'PlayChatWebHeader';
+            this.header.innerText = 'PlayChat';
+
+            this.bodyContainer = document.createElement('div');
+            this.bodyContainer.className = 'PlayChatBodyContainer';
+
+            this.messageContainer = document.createElement('div');
+            this.messageContainer.className = 'PlayChatMsgContainer';
+
+            this.messageInput = document.createElement('input');
+            this.messageInput.placeholder = '메시지를 입력해주세요';
+            this.messageContainer.appendChild(this.messageInput);
+
+            this.container.appendChild(this.header);
+            this.container.appendChild(this.bodyContainer);
+            this.container.appendChild(this.messageContainer);
+
+        };
+
+        this.init = function()
+        {
+            var body = this.iframe.contentDocument.body;
+
+            var style = document.createElement('link');
+            style.setAttribute('rel', 'stylesheet');
+            style.setAttribute('href', this.host + '/js/playchat-web/playchat-web.css');
+
+            var script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.src = this.host + '/js/playchat-web/playchat-web-core.js';
+
+            body.appendChild(style);
+            body.appendChild(this.container);
+            body.appendChild(script);
+            body.setAttribute('data-id', this.botId || '');
+            body.setAttribute('data-name', this.botName || '');
+        };
+
+    }).call(PlayChatWeb.prototype);
+
+    window.PlayChatWeb = PlayChatWeb;
+})();
