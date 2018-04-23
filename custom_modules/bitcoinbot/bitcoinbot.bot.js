cab2413141d761551fcf762b966bedd321f874b0
zsslovelyg <zsslovelyg@moneybrain.ai>
2018-04-23 14:03:51 +0900

    [Rita] bitcoinbot

--- custom_modules/bitcoinbot/bitcoinbot.bot.js
+++ custom_modules/bitcoinbot/bitcoinbot.bot.js
@@ -4,7 +4,7 @@ module.exports = function(options)
     options.use = true;
     options.kakao={
         keyboard: {
-            type:"buttons",buttons:["대화 시작"]
+            type:"buttons",buttons:["Start"]
         }
     };
 };
