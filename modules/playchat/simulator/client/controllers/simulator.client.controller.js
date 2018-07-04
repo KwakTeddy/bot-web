653b7148bf03befb3f5693af22d5d63aef57ec07
seperon <leon-moneybrain@github.com>
2018-07-04 15:56:17 +0900

    for merge

--- modules/playchat/simulator/client/controllers/simulator.client.controller.js
+++ modules/playchat/simulator/client/controllers/simulator.client.controller.js
@@ -21,10 +21,6 @@ function ($window, $scope, $cookies, $resource, $rootScope, Socket, LanguageServ
         $scope.isClosed = false;
 
         $scope.bot_image_url = chatbot.imageFile && chatbot.imageFile != '' ? chatbot.imageFile : 'modules/playchat/simulator/client/imgs/bot.png';
-        console.log('=============> boturl checker')
-        console.log($scope.bot_image_url);
-        // for test
-        //$scope.bot_image_url = 'https://s3.ap-northeast-2.amazonaws.com/playchat-files/578e5cdc06c7dc1d3e86649a/sample_restaurant_com2best_1524555556290-1528698147917-6778608613.20130306141523.jpg';
 
         $scope.$on('editChatbotInfo', function () {
             chatbot = $cookies.getObject('chatbot');
@@ -227,6 +223,9 @@ function ($window, $scope, $cookies, $resource, $rootScope, Socket, LanguageServ
         {
             try
             {
+                console.log('data type =========================send msg')
+                console.log(data)
+
                 if(data.type == 'command')
                 {
                     clearBubble();
@@ -353,7 +352,7 @@ function ($window, $scope, $cookies, $resource, $rootScope, Socket, LanguageServ
 
         $scope.init = function()
         {
-            $scope.isAdvisorMode = false;
+            $scope.isAdvisorMode = true;
             simulatorBody = angular.element('#simulatorBody');
             // init
             simulatorBody.html('');
