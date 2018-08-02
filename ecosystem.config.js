bb038bcb8821dc91018c6a3a56fd329b754cebc4
seperon <leon-moneybrain@github.com>
2018-08-02 10:25:34 +0900

    for samchully

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -29,7 +29,7 @@ module.exports =
             {
                 "PORT": 8443,
                 "HOST": "http://localhost:8443",
-                "REDIS": "127.0.0.1",
+                "REDIS": "172.31.5.26",
                 "FACEBOOK_ID" : "299548697231251",
                 "FACEBOOK_SECRET" : "f4f156d25ec93050376af77967ed500e",
                 "KAKAO_KEY": "14d5a3ad7584cf6cf2bee86dc6f34935",
