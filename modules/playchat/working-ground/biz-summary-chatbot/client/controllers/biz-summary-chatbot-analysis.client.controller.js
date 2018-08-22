8e7a8baf1384d17487766c2bbfdef663ada64d92
rita <zsslovelyg@moneybrain.ai>
2018-08-22 11:22:23 +0900

    [Rita] biz분석 쪽 추가

---
+++ modules/playchat/working-ground/biz-summary-chatbot/client/controllers/biz-summary-chatbot-analysis.client.controller.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('playchat').controller('BizSummaryChatbotAnalysisController', ['$scope', '$rootScope', '$state', '$window','$timeout', '$stateParams', '$resource', '$cookies', 'Socket','LanguageService', function ($scope, $rootScope, $state, $window, $timeout, $stateParams, $resource, $cookies, Socket, LanguageService)
+{
+    $scope.$parent.changeWorkingGroundName(LanguageService('Analysis') + ' > ' + LanguageService('Summary') + LanguageService('Chatbot'), '/modules/playchat/gnb/client/imgs/summary.png');
+    var user = $cookies.getObject('user');
+    console.log('user' + JSON.stringify(user));
+
+    $scope.User = [];
+
+    (function()
+    {
+        $scope.getList = function()
+        {
+            $scope.User.name = user.displayName;
+        };
+
+    })();
+    $scope.getList();
+    $scope.$parent.loaded('working-ground');
+    $scope.lan=LanguageService;
+}]);
