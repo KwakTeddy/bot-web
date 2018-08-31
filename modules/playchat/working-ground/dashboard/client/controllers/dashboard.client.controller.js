36a3bdea04602a19ddf40a6a0a0501a1815dd48e
seperon <leon-moneybrain@github.com>
2018-08-31 18:02:01 +0900

    push

--- modules/playchat/working-ground/dashboard/client/controllers/dashboard.client.controller.js
+++ modules/playchat/working-ground/dashboard/client/controllers/dashboard.client.controller.js
@@ -3,7 +3,7 @@
 angular.module('playchat').controller('DashboardController', ['$window', '$scope', '$cookies', '$location', 'LanguageService',function ($window, $scope, $cookies, $location, LanguageService)
 {
     $scope.$parent.changeWorkingGroundName('Dashboard', '/modules/playchat/gnb/client/imgs/dashboard_grey_1.png');
-
+    $location.url('/playchat/development/my-bot');
     var chatbot = $cookies.getObject('chatbot');
 
     $scope.chatbot = chatbot;
