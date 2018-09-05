3e877bf455e582f331a34257c43b54102a92b357
rita <zsslovelyg@moneybrain.ai>
2018-09-05 19:11:32 +0900

    [Rita] biz front 회원 profile 부분 변경 개발

--- modules/authentication/client/controllers/user-profile.client.controller.js
+++ modules/authentication/client/controllers/user-profile.client.controller.js
@@ -1,30 +1,49 @@
 (function()
 {
     'use strict';
-    angular.module('playchat').controller('UserProfileController', ['$window', '$scope', '$resource', '$cookies', '$rootScope', 'LanguageService', function ($window, $scope, $resource, $cookies, $rootScope, LanguageService)
+    angular.module('playchat').controller('UserProfileController', ['$window', '$http','$scope', '$resource', '$cookies', '$rootScope', 'LanguageService', function ($window,$http, $scope, $resource, $cookies, $rootScope, LanguageService)
     {
-        var UserService = $resource('/api/users');
-
         $scope.$parent.loading = false;
 
         var user = $cookies.getObject('user');
         $scope.user = user;
-        // $scope.language = user.language;
-
-        $scope.password = '';
-        $scope.passwordConfirm = '';
+        console.log("user: " + JSON.stringify($scope.user));
 
         (function()
         {
+            $scope.getList = function () {
+                // var bizKindOfBusiness = document.getElementById("bizKindOfBusiness");
+                // if(bizKindOfBusiness.options[bizKindOfBusiness.selectedIndex].text !== "업종을 선택하세요."){
+                //     $scope.credentials.bizKindOfBusiness = bizKindOfBusiness.options[bizKindOfBusiness.selectedIndex].text;
+                // }else{
+                //     $scope.credentials.bizKindOfBusiness = '';
+                // }
+
+
+
+               //  if($scope.user.bizKindOfBusiness === ""){
+               //      $scope.user.bizKindOfBusiness = "업종을 선택하세요.";
+               //  }
+               //
+               //  if($scope.user.bizChair === ""){
+               //      $scope.user.bizChair = "직책";
+               // }
+
+                $scope.password = '';
+                $scope.passwordConfirm = '';
+                console.log("bizKindOfBusiness: " + $scope.user.bizKindOfBusiness);
+            };
+
             $scope.save = function()
             {
-                user.language = $scope.user.language;
-
                 if($scope.password && $scope.passwordConfirm)
                 {
                     if($scope.password != $scope.passwordConfirm)
                     {
                         return alert(LanguageService('Password is disaccord'));
+                    }
+                    else{
+                        $scope.user.password = $scope.password;
                     }
                 }
                 else
@@ -32,22 +51,32 @@
                     $scope.password = '';
                 }
 
-                UserService.save({ language: $scope.user.language, password: $scope.password }, function(result)
+                if($scope.user.bizKindOfBusiness === "업종을 선택하세요." ){
+                    $scope.user.bizKindOfBusiness = "";
+                }
+
+                if($scope.bizChair === "직책"){
+                    $scope.user.bizChair = "";
+                }
+
+                $http.post('/api/users/updateBizAuthUser', $scope.user).success(function (response)
                 {
-                    // user.language = $scope.language;
+                    $window.location.href = '/playchat/development/create-bot';
                     $cookies.putObject('user', user);
                     $rootScope.$broadcast('changeLanguage');
 
                     alert(LanguageService('Has been applied'));
-                },
-                function(err)
+                }).error(function (response)
                 {
-                    alert(err);
-                });
+                    console.log(response);
+                    alert(response);
+                })
             };
+
         })();
 
         $scope.lan = LanguageService;
+        $scope.getList();
     }]);
 
 })();
